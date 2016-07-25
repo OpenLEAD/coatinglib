1f2c38c09d9d13a3c478534abaf08f324d654a36
elael <elael2@gmail.com>
2016-07-25 13:44:11 -0300

    Create Class Placement
    Class for positioning the rails and robot.

--- Python/trajectory_precision_c.py
+++ Python/trajectory_precision_c.py
@@ -1,6 +1,6 @@
 import coating
-from numpy import *
-from openravepy import *
+from numpy import array, eye, concatenate, zeros
+from openravepy import transformLookat
 from openravepy.misc import SpaceSamplerExtra
 import scipy
 from random import *
@@ -15,88 +15,101 @@ manip = robot.GetActiveManipulator()
 floor_origin = array([0,-3.22,0])
 handles=[]
 
-class robot_rail:
-    def __init__(self, env, primary_rail_name,
-                 secondary_rail_name, handles=[]):
-        self.env = env
-        self.robot = env.GetRobots()[0]
+class Placement:
+    """
+        This class provides a mecanism to generate uniform random
+        positions (in the sense described by the method) for the rail
+        and place both robot and rail system on the designed positions.
+    """
+    def __init__(self, env, robot, target, primary, secondary,
+                 floor_origin = array([0,-3.22,0]), primary_safe_margin = 0.2,
+                 secondary_safe_margin = 0.2, robot_level_difference = 0.01):
+        self._env = env
+        self._bodies = env.GetBodies()
+        self._target = target
+        self._primary = primary
+        self._secondary = secondary
+        self._robot = robot
+        self._floor_origin = floor_origin
+        self._primary_safe_margin = primary_safe_margin
+        self._secondary_safe_margin = secondary_safe_margin
+        self._robot_level_difference = robot_level_difference
+
+    def rail2xyz(self,(p,s,alpha)):
+        #P - primary rail,
+        #S - secondary rail,
+        #alpha - angle from the perpendicular to the primary rail
+        return self._floor_origin + array([p+ s*sin(alpha),
+                                           0,
+                                           s*cos(alpha)])
+
+    def place_robot(self, (p,s,alpha)):
+        Placement = eye(4)
+        #  offset HARDCODED 0.02 ref Secondary rail collision
+        Placement[0:3,3] = self.rail2xyz((p, s, alpha)) + [0, 0.02, 0]
+        R = matrixFromAxisAngle([0, alpha, 0])
+        robot.SetTransform(dot(Placement, R))
+
+            
+    def place_rail(self,(p,s,alpha)):
+        #P - primary rail,
+        #S - secondary rail,
+        #alpha - angle from the perpendicular to the primary rail
+        #ISSUE - p and s cannot be ZERO, but ignoring because random
+        #Using camera standard axis for rails
+        #Get primary and secondary from bodies
         
-
-def rail2xyz((p,s,alpha)):
-    #P - primary rail,
-    #S - secondary rail,
-    #alpha - angle from the perpendicular to the primary rail
-    return array([p+s*sin(alpha),0,s*cos(alpha)])+floor_origin
-
-def place_robot((p,s,alpha)):
-    Placement = eye(4)
-    #  offset HARDCODED 0.02 ref Secondary rail collision
-    Placement[0:3,3] = rail2xyz((p,s,alpha))+[0,0.02,0]
-    R = matrixFromAxisAngle([0, alpha, 0])
-    robot.SetTransform(dot(Placement,R))
-
+        # Primary Rail - offset HARDCODED 0.2
+        # primary = next(body for body in bodies if body.GetName()=='primary_rail')
+        primary_extent = self._primary.GetLinks()[0].GetGeometries()[0].GetBoxExtents()
+        primary_offset = array([0,-primary_extent[1],-primary_extent[2]])+array([0, 0, 0.2])
+        primary_offset_transform = eye(4)
+        primary_offset_transform[0:3,3] = primary_offset
         
-def place_rail((p,s,alpha)):
-    #P - primary rail,
-    #S - secondary rail,
-    #alpha - angle from the perpendicular to the primary rail
-    #ISSUE - p and s cannot be ZERO, but ignoring because random
-    #Using camera standard axis for rails
-    #Get primary and secondary from bodies
-    bodies = env.GetBodies()
-    
-    # Primary Rail - offset HARDCODED 0.2
-    primary = next(body for body in bodies if body.GetName()=='primary_rail')
-    primary_extent = primary.GetLinks()[0].GetGeometries()[0].GetBoxExtents()
-    primary_offset = array([0,-primary_extent[1],-primary_extent[2]])+array([0, 0, 0.2])
-    primary_offset_transform = eye(4)
-    primary_offset_transform[0:3,3] = primary_offset
-    
-    # Secondary Rail - offset HARDCODED 0.2 and 0.01
-    secondary = next(body for body in bodies if body.GetName()=='secondary_rail')
-    secondary_extent = secondary.GetLinks()[0].GetGeometries()[0].GetBoxExtents()
-    #Resizing
-    secondary_extent[2] = abs(s)/2.0 + 0.2 
-    env.RemoveKinBody(secondary)
-    secondary.InitFromBoxes(numpy.array([concatenate([zeros(3),secondary_extent])]),True)
-    env.AddKinBody(secondary)
-    #
-    secondary_offset = array([0,-secondary_extent[1],secondary_extent[2]])+array([0, 0.01, -0.2])
-    secondary_offset_transform = eye(4)
-    secondary_offset_transform[0:3,3] = secondary_offset
-    
-    # Rails Traonsform and Placement
-    primary_transform = transformLookat(floor_origin,floor_origin + array([p,0,0]),[0,1,0])
-    primary.SetTransform(dot(primary_transform,primary_offset_transform))
-    
-    secondary_transform = transformLookat(rail2xyz((p,s,alpha)),floor_origin + array([p,0,0]),[0,1,0])    
-    secondary.SetTransform(dot(secondary_transform,secondary_offset_transform))
+        # Secondary Rail - offset HARDCODED 0.2 and 0.01
+        # secondary = next(body for body in bodies if body.GetName()=='secondary_rail')
+        secondary_extent = self._secondary.GetLinks()[0].GetGeometries()[0].GetBoxExtents()
+        #Resizing
+        secondary_extent[2] = abs(s)/2.0 + 0.2 
+        env.RemoveKinBody(secondary)
+        self._secondary.InitFromBoxes(array([concatenate([zeros(3),secondary_extent])]),True)
+        env.AddKinBody(secondary)
+        #
+        secondary_offset = array([0,-secondary_extent[1],secondary_extent[2]])+array([0, 0.01, -0.2])
+        secondary_offset_transform = eye(4)
+        secondary_offset_transform[0:3,3] = secondary_offset
+        
+        # Rails Traonsform and Placement
+        primary_transform = transformLookat(floor_origin,floor_origin + array([p,0,0]),[0,1,0])
+        primary.SetTransform(dot(primary_transform,primary_offset_transform))
+        
+        secondary_transform = transformLookat(rail2xyz((p,s,alpha)),floor_origin + array([p,0,0]),[0,1,0])    
+        secondary.SetTransform(dot(secondary_transform,secondary_offset_transform))
 
-def rand_rail(N = 1):
-    #P - primary rail,
-    #S - secondary rail,
-    #alpha - angle from the perpendicular to the primary rail
-    Slimit = 2 # sencondary rail limit range
-    xmax = 1.5; xmin = -1 # Reasonable distances from the blade
-    zmax = Slimit; zmin = -Slimit
+    def rand_rail(N = 1):
+        #P - primary rail,
+        #S - secondary rail,
+        #alpha - angle from the perpendicular to the primary rail
+        Slimit = 2 # sencondary rail limit range
+        xmax = 1.5; xmin = -1 # Reasonable distances from the blade
+        zmax = Slimit; zmin = -Slimit
 
-    # Randomness
-    x,z,alpha = numpy.random.rand(3,N) 
-    # Random limits
-    x = (xmax - xmin)*x + xmin# xmin <= x < xmax - same for z
-    z = (zmax - zmin)*z + zmin
-    alphalimit = min(arccos(abs(z/Slimit)),pi/6.0) # min( Physical limits, Reasonable Angles)
-    alpha = alphalimit*(2*alpha-1) # - alphalimit <= alpha < alphalimit
+        # Randomness
+        x,z,alpha = numpy.random.rand(3,N) 
+        # Random limits
+        x = (xmax - xmin)*x + xmin# xmin <= x < xmax - same for z
+        z = (zmax - zmin)*z + zmin
+        alphalimit = min(arccos(abs(z/Slimit)),pi/6.0) # min( Physical limits, Reasonable Angles)
+        alpha = alphalimit*(2*alpha-1) # - alphalimit <= alpha < alphalimit
 
-    # S/P conversion
-    S = -z/cos(alpha)
-    P = x - S*sin(alpha)
-    
-    return (P,S,alpha)
+        # S/P conversion
+        S = -z/cos(alpha)
+        P = x - S*sin(alpha)
+        
+        return (P,S,alpha)
     
 
 # Robot Position
-#prime_rail = 
 
 # PARAMETERS
 facevector = [1,0,0]
