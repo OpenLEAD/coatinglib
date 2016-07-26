d3d0d67a980115d38195ea1ac0367f7f9dd1f3fd
elael <elael2@gmail.com>
2016-07-26 16:58:11 -0300

    class tubine - init error handling
    Error Handling for attributs initialization.

--- Python/turbine.py
+++ Python/turbine.py
@@ -1,13 +1,13 @@
 import coating
-from numpy import array, cross
-from openravepy import transformLookat
-from openravepy.misc import SpaceSamplerExtra
-import scipy
-from random import *
-import sys
+from numpy import array, cross, dot
+from openravepy import Environment
+##from openravepy.misc import SpaceSamplerExtra
+##import scipy
+##from random import *
+##import sys
 
 env=Environment()
-env.SetViewer('qtcoin')
+#env.SetViewer('qtcoin')
 env.Load("../Turbina/env_mh12_0_16.xml")
 robot = env.GetRobots()[0]
 target = env.GetBodies()[0]
@@ -17,20 +17,60 @@ handles=[]
 
 _PRIMARY_RAIL = "primary_rail"
 _SECONDARY_RAIL = "secondary_rail"
+_BLADE = "pa"
+_RUNNER_AREA = "arocamara"
+_IRIS = "iris"
+
+
+class XMLStructError(Exception):    
+    def __init__(self, value):
+        Exception.__init__(self,"No body named " + value + " found.")
 
 
 class Turbine:
+    
+    std_name = { _PRIMARY_RAIL: "primary rail",
+                 _SECONDARY_RAIL: "secondary rail",
+                 _BLADE: "blade",
+                 _RUNNER_AREA: "runner area"
+                 _IRIS: "iris"
+                 }
 
     def __init__(self,env,floor_origin,nose_axis):
-        self._env = env
-        self._primary = next(body for body in bodies if body.GetName()==_PRIMARY_RAIL)
-        self._secondary = next(body for body in bodies if body.GetName()==_SECONDARY_RAIL)
+        self.env = env
+        bodies = env.GetBodies()
+            
         self._floor_origin = floor_origin
         self._floor_axis = array([nose_axis, cross(floor_origin,nose_axis)])
-        
 
-    def floor2xyz( v ):
-        return v
-    
-    def xyz2floor( v ):
-        return v
+        
+        try:
+            self.primary = next(body for body in bodies if body.GetName()==_PRIMARY_RAIL)
+        except StopIteration:
+            raise XMLStructError(_PRIMARY_RAIL)
+
+        try:
+            self.secondary = next(body for body in bodies if body.GetName()==_SECONDARY_RAIL)
+        except StopIteration:
+            raise XMLStructError(_SECONDARY_RAIL)
+
+        blade_number = 1
+        try:
+            self.blades = [next(body for body in bodies if body.GetName()==_BLADE+str(blade_number))]
+        except StopIteration:
+            raise XMLStructError(_BLADE+str(blade_number))
+
+        blade_found = True
+        while blade_found:
+            blade_number += 1
+            try:
+                self.blades.append(next(body for body in bodies if body.GetName()==_BLADE+str(blade_number)))
+            except StopIteration:
+                blade_found = False
+
+
+    def getFloorOrigin(self):
+        return array(self._floor_origin)
+
+    def getFloorAxis(self):
+        return array(self._floor_axis)
