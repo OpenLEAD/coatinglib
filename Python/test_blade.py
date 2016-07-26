0180fdbac37a70004c7e0a6b8f89515d9b36e06f
renan028 <renan028@gmail.com>
2016-07-26 15:29:26 -0300

    test make_model is working

--- Python/test_blade.py
+++ Python/test_blade.py
@@ -2,13 +2,21 @@ from openravepy import Environment
 import RBF_c
 import blade_c
 import math
+import mathtools
+
 
 env=Environment()
 env.Load("../Turbina/env_mh12_0_16.xml")
-
+env.SetViewer('qtcoin')
 
 rbf = RBF_c.RBF('jiraublade','r3')
 
-blade = blade_c.Blade('jiraublade', env)
+blade = blade_c.Blade('jiraublade', env, 'pa1')
 blademodel = blade_c.BladeModeling('jiraublade', rbf, env, blade.blade)
-blademodel.sampling(bladerotation=[-27*math.pi/180,'y'])
+
+##blademodel.sampling(bladerotation=[-27*math.pi/180,'y'])
+##
+#blademodel.make_model()
+
+sphere = mathtools.sphere()
+blademodel.generate_trajectory(sphere)
