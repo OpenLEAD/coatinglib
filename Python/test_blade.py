45fe7621ef91ea971870043abbbcca4773322648
renan028 <renan028@gmail.com>
2016-08-08 11:57:40 -0300

    modify nose radius

--- Python/test_blade.py
+++ Python/test_blade.py
@@ -1,22 +1,23 @@
-import RBF_c
-import blade_c
+import RBF
+import blade
 import math
 import mathtools
 from turbine import Turbine
 
+name = "jiraublade_t"
 
-rbf = RBF_c.RBF('jiraublade','r3')
+rbf = RBF.RBF(name,'r3')
 
 turbine = Turbine('turbine_std.cfg', False)
 
-blademodel = blade_c.BladeModeling('jiraublade', rbf, turbine, True)
+blademodel = blade.BladeModeling(name, rbf, turbine, True)
 
 blademodel.sampling()
 ##
-blademodel.make_model()
+#blademodel.make_model()
 
-sphere = mathtools.sphere(turbine.model.runner_radius,
-                          turbine.model.nose_radius,
-                          turbine.coating.parallel_gap
-                          )
-blademodel.generate_trajectory(sphere)
+#sphere = mathtools.sphere(turbine.model.runner_radius,
+#                          turbine.model.nose_radius,
+#                          turbine.coating.parallel_gap
+#                          )
+#blademodel.generate_trajectory(sphere)
