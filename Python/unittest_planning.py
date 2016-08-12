c595dd8d9e6deacb45a50219c0c750aa717c85ce
renan028 <renan028@gmail.com>
2016-08-12 17:19:04 -0300

    create cylinder in planning unittest

--- Python/unittest_planning.py
+++ Python/unittest_planning.py
@@ -2,7 +2,7 @@ import planning
 import unittest
 from numpy import linspace, cos, sin, arange, zeros, ones, random, array, array_equal, dstack
 from math import pi
-import copy
+from copy import deepcopy
 
 a = []
 b = []
@@ -10,6 +10,7 @@ b = []
 class TestPlanning(unittest.TestCase):
 
     def setUp(self):
+        global a
         r = 1.43
         self.B = array([2.43, 0, -1])
         theta = linspace(-10,10,15)*pi/180
@@ -19,29 +20,21 @@ class TestPlanning(unittest.TestCase):
         nxny = dstack((-y,x))
         z = arange(-0.05, 0.05, 0.003)
         nxnynz = dstack((nxny,zeros((len(x),))))
-        print nxnynz
         N = len(theta)
         self.trajectories = []
         for zi in z:
             zr = ones((len(x),))*zi
             xyz = dstack((xy,zr))
             trajectory = zeros((N,6))
-            trajectory[:,0] = x
-            trajectory[:,1] = y
-            trajectory[:,2] = zr
-            trajectory[:,3] = -y
-            trajectory[:,4] = x
-            trajectory[:,5] = nz
-            trajectory = [
-            self.trajectories.append(trajectory)  
-        self.orgtraj = copy.copy(self.trajectories)
+            trajectory[:,0:3] = xyz
+            trajectory[:,3:6] = nxnynz
+            self.trajectories.append(trajectory)
+        a = deepcopy(self.trajectories)
         for traj in self.trajectories:
            random.shuffle(traj) 
              
     def test_sortTrajectories(self):
-        global a
         global b
-        a = self.orgtraj
         b = planning.sortTrajectories2(self.trajectories)
         self.assertTrue(array_equal(self.orgtraj,
                                     planning.sortTrajectories2(self.trajectories)))             
