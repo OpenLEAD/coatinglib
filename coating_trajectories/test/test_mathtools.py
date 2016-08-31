e80a7bdfb2f8497cb0e32e455ad66d19160f8f68
renan028 <renan028@gmail.com>
2016-08-31 14:25:51 -0300

    Finish test_mathtools and test_rbf

--- coating_trajectories/test/test_mathtools.py
+++ coating_trajectories/test/test_mathtools.py
@@ -1,10 +1,11 @@
 import unittest
-from numpy import array, array_equal, eye, pi, around, sqrt
+from numpy import array, array_equal, eye, pi, around, sqrt, random
 from math import cos, sin
-
 from .. import mathtools
+from . import TestCase
 
-class Testmathtools(unittest.TestCase):
+class Testmathtools(TestCase):
+
     def test_hat(self):
         v = [1, 2, 3]
         M = array([[0, -3, 2],
@@ -25,18 +26,22 @@ class Testmathtools(unittest.TestCase):
         self.assertTrue(array_equal(around(mathtools.Raxis(array([1, 0, 0]), pi/2),5), M))
 
     def test_surfaces_tangent(self):
-        s = mathtools.sphere(Rn0=3)
+        s = mathtools.Sphere(Rn0=3)
         ray = array([3, 0, 0, 0, 0, 1])
         tan = mathtools.surfaces_tangent(ray, s)
         self.assertTrue(array_equal(array([0, 1, 0]), tan))
 
     def test_curvepoint(self):
+        """
+        The test creates an initial point with some disturbance.
+        For the surfaces, the test creates a sphere with radius = 2, and center = (0,0,0); and a plane z=-1.
+        The intersection between the surfaces is a known circle with radius = sqrt(3).
+        The test verifies if the computed point belongs to sphere and plane.
+        """
         s = mathtools.Sphere(2, 0, 0.001)
-        data = random.rand(100,6)
-        for point in data:
-            point[3:6] = point[3:6]/sqrt(dot(point[3:6],point[3:6]))
-            point[0:3] = point[0:3]/sqrt(dot(point[0:3],point[0:3]))
-        data[:,2]-=1
+        initial_point = array([sqrt(3), 0, -1, 0, 0, 1])
+        disturbance = array([float(random.rand(1)*1e-3), 0, 0, 0, 0, 0] )
+        initial_point = initial_point + disturbance
         
         class ZPlane:
             def __init__(self, z0=-1):
@@ -47,18 +52,12 @@ class Testmathtools(unittest.TestCase):
                 return array([0, 0, 1])
         zp = ZPlane()
         
-        self.blade3._points = data
-        self.blade3._model = zp
-        initial_point = data[argmax(s.f_array(data))]
         s.findnextparallel(initial_point)
-        initial_point = curvepoint(zp, s, p)
+        initial_point = mathtools.curvepoint(zp, s, initial_point[0:3])
 
         # Testing if point belongs to the sphere and plane
         self.assertTrue(abs(s.f(initial_point))<=1e-5, msg = 'point ='+str(abs(s.f(initial_point)))+' does not belong to sphere')
         self.assertTrue(abs(zp.f(initial_point))<=1e-5, msg = 'point ='+str(abs(s.f(initial_point)))+' does not belong to plane')
-        
-
-        
 
 if __name__ == '__main__':
     unittest.main()
--- coating_trajectories/test/test_rbf.py
+++ coating_trajectories/test/test_rbf.py
@@ -1,5 +1,5 @@
 import os
-from numpy import array, load, abs, max, mean, sum, min
+from numpy import array, load, abs, max, mean, sum, min, sqrt, random
 
 import unittest
 from . import TestCase
@@ -12,123 +12,147 @@ cosmean = 0.92
 
 class TestRBF(TestCase):
 
-    
     def setUp(self):
-        template_points = load(self.test_dir + '/template/points.npz')
-        template_points = template_points['array']
-        
-        name = "test"
-        rbf_r3 = rbf.RBF(name, 'r3', template_points)
-        rbf_logr = rbf.RBF(name, 'logr', template_points)
-        rbf_gaussr = rbf.RBF(name, 'gaussr', template_points)
-        
-        self.template_points = load(self.test_dir + '/template/extra_points.npz')
-        self.template_points = self.template_points['array']
-        self.template_normal = self.template_points[:,3:6]
+        """
+        Making a sphere with random points for the RBFs.
+        """
+        number_of_model_data = 4000
+        model_data = random.rand(number_of_model_data,6)
+        model_data[:,0:3] = model_data[:,0:3]*(1.0/(sqrt(sum(model_data[:,0:3]*model_data[:,0:3],1)))).reshape(number_of_model_data,1)
+        model_data[:,3] = model_data[:,0]*2
+        model_data[:,4] = model_data[:,1]*2
+        model_data[:,5] = model_data[:,2]*2
+        self.model_data = model_data
+
+        number_of_validate_data = 100
+        validate_data = random.rand(number_of_validate_data,6)
+        validate_data[:,0:3] = validate_data[:,0:3]*(1.0/(sqrt(sum(validate_data[:,0:3]*validate_data[:,0:3],1)))).reshape(number_of_validate_data,1)
+        validate_data[:,3] = validate_data[:,0]*2
+        validate_data[:,4] = validate_data[:,1]*2
+        validate_data[:,5] = validate_data[:,2]*2
+        self.validation_data = validate_data
+        self.normal_validation_data = validate_data[:,3:6]
 
     def test_r3(self):
-        TestRBF.rbf_r3.make()
+        """
+        The test generates the RBF cubic kernel model with model_data samples,
+        and computes the function for validation_data points. If mean and max values,
+        outside points, and the normal vectors are in a threshold, the test passes. 
+        """
+        name = "test"
+        rbf_r3 = rbf.RBF(name, 'r3', self.model_data)
+        rbf_r3.make()
 
+        # Verifying if validation_data is right
         rbf_results = []
-        for point in self.template_points:
-            rbf_results.append(TestRBF.rbf_r3.f(point[0:3]))
-
+        for point in self.validation_data:
+            rbf_results.append(rbf_r3.f(point[0:3]))
+        rbf_results = array(rbf_results)
         rbf_results = abs(rbf_results)
-
         maxvalue = max(rbf_results)
         meanvalue = mean(rbf_results)
-
         self.assertTrue(maxvalue<tolmax and meanvalue<tolmean,
-                        msg='max = '+str(maxvalue)+', mean = '+ str(meanvalue))
+                        msg = "max(validation_data) ="+str(maxvalue)+", it should be "+str(tolmax)+"mean(validation_data) ="+str(meanvalue)+", it should be "+str(tolmean))
 
-        outside_points = TestRBF.rbf_r3._pointsaugment(self.template_points)
-        rbf_outside_results = []
+        # Verifying if outside_points are right
+        outside_points = rbf_r3._pointsaugment(self.validation_data)
+        rbf_results = []
         for point in outside_points:
-            rbf_outside_results.append(TestRBF.rbf_r3.f(point[0:3]))
-        rbf_outside_results = rbf_outside_results
-        self.assertTrue(mean(rbf_outside_results)>TestRBF.rbf_r3._eps*(1-toleps),
-                        msg = str(mean(rbf_outside_results)))
+            rbf_results.append(rbf_r3.f(point[0:3]))
+        rbf_results = array(rbf_results)
+        self.assertTrue(abs(mean(rbf_results)-rbf_r3._eps)<=toleps,
+                        msg = "mean(outside_points) ="+str(mean(rbf_results))+", it should be "+str(rbf_r3._eps))
 
-        rbf_normal = []
-        for point in self.template_points:
-            rbf_normal.append(TestRBF.rbf_r3.df(point[0:3]))
-        rbf_normal = array(rbf_normal)
-        cos_theta = sum(rbf_normal*self.template_normal,1)
-
+        # Verifying if normal_vectors are right
+        rbf_results = []
+        for point in self.validation_data:
+            rbf_results.append(rbf_r3.df(point[0:3]))
+        rbf_results = array(rbf_results)
+        cos_theta = sum(rbf_results*self.normal_validation_data,1)
         cos_theta_min = min(cos_theta)
         cos_theta_mean = mean(cos_theta)
-        
         self.assertTrue(cos_theta_mean>cosmean,
                         msg='mean = '+ str(cos_theta_mean))
         
+    def test_gaussr(self):
+        """
+        The test generates the RBF gaussian kernel model with model_data samples,
+        and computes the function for validation_data points. If mean and max values,
+        outside points, and the normal vectors are in a threshold, the test passes. 
+        """
+        name = "test"
+        rbf_gaussr = rbf.RBF(name, 'gaussr', self.model_data)
+        rbf_gaussr.make()
+
+        # Verifying if validation_data is right
+        rbf_results = []
+        for point in self.validation_data:
+            rbf_results.append(rbf_gaussr.f(point[0:3]))
+        rbf_results = array(rbf_results)
+        rbf_results = abs(rbf_results)
+        maxvalue = max(rbf_results)
+        meanvalue = mean(rbf_results)
+        self.assertTrue(maxvalue<tolmax and meanvalue<tolmean,
+                        msg = "max(validation_data) ="+str(maxvalue)+", it should be "+str(tolmax)+"mean(validation_data) ="+str(meanvalue)+", it should be "+str(tolmean))
+
+        # Verifying if outside_points are right
+        outside_points = rbf_gaussr._pointsaugment(self.validation_data)
+        rbf_results = []
+        for point in outside_points:
+            rbf_results.append(rbf_gaussr.f(point[0:3]))
+        rbf_results = array(rbf_results)
+        self.assertTrue(abs(mean(rbf_results)-rbf_gaussr._eps)<=toleps,
+                        msg = "mean(outside_points) ="+str(mean(rbf_results))+", it should be "+str(rbf_gaussr._eps))
+
+        # Verifying if normal_vectors are right
+        rbf_results = []
+        for point in self.validation_data:
+            rbf_results.append(rbf_gaussr.df(point[0:3]))
+        rbf_results = array(rbf_results)
+        cos_theta = sum(rbf_results*self.normal_validation_data,1)
+        cos_theta_min = min(cos_theta)
+        cos_theta_mean = mean(cos_theta)
+        self.assertTrue(cos_theta_mean>cosmean,
+                        msg='mean = '+ str(cos_theta_mean))
 
     def test_logr(self):
-        TestRBF.rbf_logr.make()
-       
+        """
+        The test generates the RBF logarithmic kernel model with model_data samples,
+        and computes the function for validation_data points. If mean and max values,
+        outside points, and the normal vectors are in a threshold, the test passes. 
+        """
+        name = "test"
+        rbf_logr = rbf.RBF(name, 'logr', self.model_data)
+        rbf_logr.make()
+
+        # Verifying if validation_data is right
         rbf_results = []
-        for point in self.template_points:
-            rbf_results.append(TestRBF.rbf_logr.f(point[0:3]))
-
+        for point in self.validation_data:
+            rbf_results.append(rbf_logr.f(point[0:3]))
+        rbf_results = array(rbf_results)
         rbf_results = abs(rbf_results)
-
         maxvalue = max(rbf_results)
         meanvalue = mean(rbf_results)
-
         self.assertTrue(maxvalue<tolmax and meanvalue<tolmean,
-                        msg='max = '+str(maxvalue)+', mean = '+ str(meanvalue))
+                        msg = "max(validation_data) ="+str(maxvalue)+", it should be "+str(tolmax)+"mean(validation_data) ="+str(meanvalue)+", it should be "+str(tolmean))
 
-        outside_points = TestRBF.rbf_logr._pointsaugment(self.template_points)
-        rbf_outside_results = []
-        for point in outside_points:
-            rbf_outside_results.append(TestRBF.rbf_logr.f(point[0:3]))
-        rbf_outside_results = rbf_outside_results
-        self.assertTrue(mean(rbf_outside_results)>TestRBF.rbf_logr._eps*(1-toleps),
-                        msg = str(mean(rbf_outside_results)))
-
-        rbf_normal = []
-        for point in self.template_points:
-            rbf_normal.append(TestRBF.rbf_logr.df(point[0:3]))
-        rbf_normal = array(rbf_normal)
-        cos_theta = sum(rbf_normal*self.template_normal,1)
-
-        cos_theta_min = min(cos_theta)
-        cos_theta_mean = mean(cos_theta)
-        
-        self.assertTrue(cos_theta_mean>cosmean,
-                        msg='mean = '+ str(cos_theta_mean))
-
-    def test_gaussr(self):
-        TestRBF.rbf_gaussr.make()
-
+        # Verifying if outside_points are right
+        outside_points = rbf_logr._pointsaugment(self.validation_data)
         rbf_results = []
-        for point in self.template_points:
-            rbf_results.append(TestRBF.rbf_gaussr.f(point[0:3]))
-
-        rbf_results = abs(rbf_results)
-
-        maxvalue = max(rbf_results)
-        meanvalue = mean(rbf_results)
-
-        self.assertTrue(maxvalue<tolmax and meanvalue<tolmean,
-                        msg='max = '+str(maxvalue)+', mean = '+ str(meanvalue))
-
-        outside_points = TestRBF.rbf_gaussr._pointsaugment(self.template_points)
-        rbf_outside_results = []
         for point in outside_points:
-            rbf_outside_results.append(TestRBF.rbf_gaussr.f(point[0:3]))
-        rbf_outside_results = rbf_outside_results
-        self.assertTrue(mean(rbf_outside_results)>TestRBF.rbf_gaussr._eps*(1-toleps),
-                        msg = str(mean(rbf_outside_results)))
+            rbf_results.append(rbf_logr.f(point[0:3]))
+        rbf_results = array(rbf_results)
+        self.assertTrue(abs(mean(rbf_results)-rbf_logr._eps)<=toleps,
+                        msg = "mean(outside_points) ="+str(mean(rbf_results))+", it should be "+str(rbf_logr._eps))
 
-        rbf_normal = []
-        for point in self.template_points:
-            rbf_normal.append(TestRBF.rbf_gaussr.df(point[0:3]))
-        rbf_normal = array(rbf_normal)
-        cos_theta = sum(rbf_normal*self.template_normal,1)
-
+        # Verifying if normal_vectors are right
+        rbf_results = []
+        for point in self.validation_data:
+            rbf_results.append(rbf_logr.df(point[0:3]))
+        rbf_results = array(rbf_results)
+        cos_theta = sum(rbf_results*self.normal_validation_data,1)
         cos_theta_min = min(cos_theta)
         cos_theta_mean = mean(cos_theta)
-        
         self.assertTrue(cos_theta_mean>cosmean,
                         msg='mean = '+ str(cos_theta_mean))
 
