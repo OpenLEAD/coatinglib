419ed4ff2a97cdd68945b78a943bb3bf8287786e
renan028 <renan028@gmail.com>
2016-08-31 10:35:22 -0300

    modify test of curvepoint

--- coating_trajectories/test/test_mathtools.py
+++ coating_trajectories/test/test_mathtools.py
@@ -31,23 +31,33 @@ class Testmathtools(unittest.TestCase):
         self.assertTrue(array_equal(array([0, 1, 0]), tan))
 
     def test_curvepoint(self):
-        s1 = mathtools.sphere(Rn0=3)
-        class plane:
-            def __init__(self, y0=3.770):
-                self.y = y0
+        s = mathtools.Sphere(2, 0, 0.001)
+        data = random.rand(100,6)
+        for point in data:
+            point[3:6] = point[3:6]/sqrt(dot(point[3:6],point[3:6]))
+            point[0:3] = point[0:3]/sqrt(dot(point[0:3],point[0:3]))
+        data[:,2]-=1
+        
+        class ZPlane:
+            def __init__(self, z0=-1):
+                self.z = z0
             def f(self, p):
-                return p[1]-self.y
+                return p[2]-self.z
             def df(self, p):
-                return array([0, 1, 0])
-        s2 = plane(0)    
-        self.assertTrue(array_equal(array([0, 0, 3, 0, 0, 1]),
-                                    around(mathtools.curvepoint(s1,
-                                                                s2,
-                                                                array([0, 0, 3.1])
-                                                                )
-                                           )
-                                    )
-                        )
+                return array([0, 0, 1])
+        zp = ZPlane()
+        
+        self.blade3._points = data
+        self.blade3._model = zp
+        initial_point = data[argmax(s.f_array(data))]
+        s.findnextparallel(initial_point)
+        initial_point = curvepoint(zp, s, p)
+
+        # Testing if point belongs to the sphere and plane
+        self.assertTrue(abs(s.f(initial_point))<=1e-5, msg = 'point ='+str(abs(s.f(initial_point)))+' does not belong to sphere')
+        self.assertTrue(abs(zp.f(initial_point))<=1e-5, msg = 'point ='+str(abs(s.f(initial_point)))+' does not belong to plane')
+        
+
         
 
 if __name__ == '__main__':
