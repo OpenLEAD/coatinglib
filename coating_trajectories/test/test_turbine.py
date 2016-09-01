e2b73a71f6233619e71bcdd7f49dfc53596979ef
elael <elael2@gmail.com>
2016-09-01 11:19:57 -0300

    CREATE test turbine
    Simple test, checks creation.

---
+++ coating_trajectories/test/test_turbine.py
@@ -0,0 +1,14 @@
+import unittest
+from . import TestCase
+from ..turbine_config import TurbineConfig, ConfigFileError
+from ..turbine import Turbine
+
+class TestTurbine(TestCase):
+
+    def test_creation(self):
+        turbconf = TurbineConfig.load("/turbine_unittest.cfg",TestTurbine.test_dir)
+        turb = Turbine(turbconf,False)
+        
+
+if __name__ == '__main__':
+    unittest.main()
