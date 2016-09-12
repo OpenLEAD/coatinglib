ea847ead4bd286bd8bbfca14994f5b1ff627dc4d
renan028 <renan028@gmail.com>
2016-09-12 09:12:44 -0300

    Create new function: planning
    The script's functionalities are:
    - check robot dof limits
    - ikfast (openrave)
    - full inverse kinematics with coating angle and distance tolerance
    - orientation error optimization
    - compute robot joints for given trajectory
    The test planning tests the methods above.

---
+++ coating_trajectories/planning.py
@@ -0,0 +1,199 @@
+from numpy import sqrt, dot, concatenate, arange, array, zeros, transpose
+from openravepy import IkFilterOptions
+from math import pi, cos, sin, atan2
+from scipy.optimize import minimize
+from copy import copy
+import mathtools
+
+""" Main package for robot joints' positions and velocities planning,
+robot base calculation, torque and manipulability analysis.
+
+Keyword arguments:
+places - robot places to coat a specific set of parallels
+turbine - full environment and parameters
+"""
+
+
+def _std_robot_filter(turbine, trajectories):
+    raise ValueError("No trajectory filter for "+turbine.robot.GetName()+" robot. Create new function.")
+
+def mh12_filter(turbine, trajectories):
+    def distance_robot(point,turbine):
+        pass # TODO
+    filtered_trajectories = []
+    for jectory in trajectories:
+        
+        filtered_jectory = []
+        for point in jectory:
+            
+            if distance_robot(point[0:3],turbine):
+                pass #TODO
+    
+_filter_options = {'mh12': mh12_filter}
+def filter_trajectories(turbine, trajectories):
+    name = turbine.robot.GetName()
+    _filter_options.get(name,_std_robot_filter)(turbine, trajectories)
+
+def compute_robot_joints(turbine, trajectory):
+    """
+    Iterates points of the trajectories, computing optimal robot's joints
+    (minimizing orientation error).
+
+    Keyword arguments:
+    place -- robot position
+    turbine -- tubine object
+    """
+
+    robot = turbine.robot
+    q0, _ = inverse_kinematics(turbine, trajectory[0])
+    try:
+        q0 = q0[0]
+    except IndexError:
+        raise IndexError('There is no solution for the given trajectory.')
+    
+    robot.SetDOFValues(q0)
+    joint_solutions = []
+    
+    for point in trajectory:
+        res = orientation_error_optimization(turbine, point)
+        if not res.success: return joint_solutions 
+        else:
+            joint_solutions.append(res.x)
+            robot.SetDOFValues(res.x)
+    return joint_solutions
+
+def orientation_error_optimization(turbine, point):
+    """ Minimizes the orientation error, given an initial configuration of the robot
+    and the desired point to coat (with its normal vector). The constraints of the minimi-
+    zation are: to reach de point position (equality), to be in coating angle tolerance
+    (inequality).
+
+    Keyword arguments:
+    robot -- the robot.
+    point -- 6x1 vector is the goal (point to be coated). (x,y,z) cartesian position
+    and (nx,ny,nz) is the normal vector of the point, w.r.t. the world frame.
+    """
+    #TODO check collision
+    robot = turbine.robot
+    cos_tolerance = cos(turbine.config.coating.angle_tolerance)
+    q0 = robot.GetDOFValues()
+    manip = robot.GetActiveManipulator()
+    lower_limits, upper_limits = robot.GetActiveDOFLimits()
+    with robot:
+        def func(q):
+            robot.SetDOFValues(q, manip.GetArmIndices())
+            T = manip.GetTransform()
+            Rx = dot(manip.GetDirection(), T[0:3,0:3])
+            Rx = Rx/sqrt(dot(Rx,Rx))
+            return -dot(point[3:6], Rx)
+        def position_cons(q):
+            robot.SetDOFValues(q, manip.GetArmIndices())
+            T = manip.GetTransform()
+            pos = T[0:3,3]
+            dif = pos-point[0:3]
+            return dot(dif,dif)
+        def orientation_cons(q):
+            robot.SetDOFValues(q, manip.GetArmIndices())
+            T = manip.GetTransform()
+            Rx = dot(manip.GetDirection(), T[0:3,0:3])
+            Rx = Rx/sqrt(dot(Rx,Rx))
+            return dot(point[3:6], Rx) - cos_tolerance
+        cons = ({'type':'eq', 'fun': position_cons},
+                {'type':'ineq', 'fun': orientation_cons})
+        bnds = tuple([(lower_limits[i],upper_limits[i]) for i in range(0,len(lower_limits))])
+        res = minimize(func, q0, constraints=cons, method='SLSQP',
+                       bounds = bnds, options={'disp': False})
+    return res    
+
+def Qvector_backtrack(y, Q):
+    """ Call when optimization fails. Previous solution should be recomputed.
+
+    Keyword arguments:
+    y -- computed coated points.
+    Q -- computed robot's joints.
+    """
+    for rev_y in reversed(y):
+        res = coating.optmizeQ(turbine, rev_y, Q[-1])
+        Q.append(res.x)
+        if not res.success:
+            print 'Qvector_backtrack error'
+    return list(reversed(Q))
+
+def inverse_kinematics(turbine, point):
+    """
+    Solve the inverse kinematics given point (IKFast).
+
+    Keyword arguments:
+    turbine -- turbine object
+    point -- point to be coated is a 6D array, which (x,y,z) cartesian position
+    and (nx,ny,nz) is the normal vector of the point, w.r.t. the world frame.
+    """
+
+    robot = turbine.robot
+    manip = robot.GetActiveManipulator()
+    coating_tolerance_max = turbine.config.coating.max_distance
+    coating_tolerance_min = turbine.config.coating.min_distance
+
+    # Compute solution without tolerances
+    iksol = ikfast(robot, point)
+    if len(iksol)>0:return iksol, []
+
+    # Compute solution with distance and angle tolerances
+    angles = []
+    if turbine.config.coating.angle_tolerance>0:
+        angles = arange(0, turbine.config.coating.angle_tolerance, 0.001)
+    number_of_angles = 10
+
+    if (coating_tolerance_max!=0) or (coating_tolerance_min!=0):
+        for distance in arange(coating_tolerance_min, coating_tolerance_max, 1e-3):
+            new_point = concatenate((point[0:3]+distance*point[3:6], point[3:6]))
+            iksol = ikfast(robot, new_point)
+            if len(iksol)>0: return iksol, ['distance', distance] 
+            if len(angles)>0:
+                for angle in angles:
+                    Rz = mathtools.Raxis(mathtools.compute_perpendicular_vector(manip.GetDirection()),
+                                         angle)
+                    p = dot(manip.GetDirection(), transpose(Rz))
+                    k = 2*pi/number_of_angles
+                    for i in range(0, number_of_angles):
+                        alfa = k*i
+                        Rd = mathtools.Raxis(manip.GetDirection(),alfa)
+                        iksol = ikfast(robot,
+                                       concatenate((point[0:3],dot(p, transpose(Rd)))))
+                        if len(iksol)>0:return iksol, ['angle', angle]     
+    return [], []
+            
+def ikfast(robot, point):
+    """
+    Call openrave IKFast. It computes the inverse kinematic for the point.
+    It returns all solutions.
+
+    keyword arguments:
+    robot -- the robot. 
+    point -- point to coat is a 6D array, which (x,y,z) cartesian position
+    and (nx,ny,nz) is the normal vector of the point, w.r.t. the world frame.
+    """
+    with robot:
+        manip = robot.GetActiveManipulator()
+        T = zeros((4,4))
+        T[0:3,0:3] = mathtools.Rab(manip.GetDirection(), point[3:6])
+        T[0:3,3] = point[0:3]
+        T[3,0:4] = [0,0,0,1]
+        return robot.GetActiveManipulator().FindIKSolutions(T, IkFilterOptions.CheckEnvCollisions)
+
+def check_dof_limits(robot, q):
+    """
+    Method checks if the joints limits are inside limits with a safe tolerance.
+    return True if limits were respected.
+    keyword arguments:
+    robot -- the robot. 
+    q -- DOF values.
+    """
+    joints = robot.GetJoints()
+    for i in range(0,len(q)):
+        l,u = joints[i].GetLimits()
+        l = l[0]
+        u = u[0]
+        if not q[i]>=l+0.01 or not q[i]<=u-0.01:
+            return False
+    return True    
---
+++ coating_trajectories/test/test_planning.py
@@ -0,0 +1,142 @@
+import unittest
+from . import TestCase
+from .. turbine import Turbine
+from .. turbine_config import TurbineConfig, ConfigFileError
+from .. import planning
+from numpy import concatenate, array, random, arange, dot, sqrt, sin, cos, ones, sum, zeros
+from math import pi
+from openravepy import RaveCreateCollisionChecker, CollisionOptions, CollisionReport
+
+class TestPlanning(TestCase):
+
+    def setUp(self):
+        self.path = "turbine_unittest.cfg"
+
+    def test_ikfast(self):
+        """
+        The test verifies if the robot can reach a reachable point, a reachable point
+        with the blade as an obstacle (flame can reach it as there is no collision),
+        and a non reachable point. The RaveCreateCollisionChecker('ode') do not check
+        collisions with an initial state, only timestep positions.
+        """
+        turbconf = TurbineConfig.load(self.path, self.test_dir)
+        turb = Turbine(turbconf)
+        turb.robot.GetLink('Flame').Enable(True)
+        turb.env.SetCollisionChecker(RaveCreateCollisionChecker(turb.env,'ode'))
+        
+        
+        # Reachable point
+        point = turb.manipulator.GetTransform()[0:3,3]
+        disturbance = random.uniform(-0.01,0.01,3)
+        point = point + disturbance
+        point = concatenate((point,[1,0,0]))
+        iksol = planning.ikfast(turb.robot, point)
+        self.assertTrue(len(iksol)>0, msg = 'No solution was found')
+
+        # Reachable point with blade as an obstacle
+        blade_T = turb.robot.GetTransform()
+        blade_T[0:3,3] = turb.manipulator.GetTransform()[0:3,3]+[0, 2, 0]
+        turb.blades[0].SetTransform(blade_T)
+        iksol = planning.ikfast(turb.robot, point)
+        self.assertTrue(len(iksol)>0, msg = 'No solution was found')
+
+        # Non-reachable point
+        point[0] += -0.2
+        iksol = planning.ikfast(turb.robot, point)
+        self.assertTrue(len(iksol)==0, msg = 'A solution was found')
+
+    def test_inverse_kinematics(self):
+        """
+        The test starts with a reachable point and modify it by a small step until the robot
+        uses the tolerance to coat it. If there is no tolerance, the test will fail.
+        """
+        
+        turbconf = TurbineConfig.load(self.path, self.test_dir)
+        turb = Turbine(turbconf)
+        
+        step = 1e-3
+        point = turb.manipulator.GetTransform()[0:3,3]
+        point = concatenate((point,[1,0,0]))
+        iksol,_ = planning.inverse_kinematics(turb, point)
+        tolerance_verification = False
+
+        while len(iksol)>0:
+            turb.robot.SetDOFValues(iksol[0])
+            point[0:3] = point[0:3]+step
+            iksol, tolerance = planning.inverse_kinematics(turb, point)
+            if tolerance:
+                tolerance_verification = True
+                break
+        self.assertTrue(tolerance_verification, 'Tolerances could not be verified')
+
+    def test_check_dof_limits(self):
+        """
+        Test a feasible and a non-feasible configuration of joint values. 
+        """
+        turbconf = TurbineConfig.load(self.path, self.test_dir)
+        turb = Turbine(turbconf)
+        robot = turb.robot
+        q = robot.GetActiveDOFValues()
+        self.assertTrue(planning.check_dof_limits(robot, q),
+                        msg= 'DOF outside limits')
+
+        q[0] = robot.GetJoints()[0].GetLimits()[0]
+        self.assertTrue(~planning.check_dof_limits(robot, q),
+                        msg='DOF inside limits')
+
+    def test_orientation_error_optimization(self):
+        """
+        Test a reachable point. 
+        """
+        turbconf = TurbineConfig.load(self.path, self.test_dir)
+        turb = Turbine(turbconf)
+
+        lower_limits, upper_limits = turb.robot.GetActiveDOFLimits()
+        q = []
+        for joint_index in range(0,len(lower_limits)):
+            safe_region = (upper_limits[joint_index]+lower_limits[joint_index])/2
+            q.append(random.uniform(safe_region-safe_region*1e-5,
+                                    safe_region+safe_region*1e-5))
+
+        turb.robot.SetDOFValues(q)
+        Tee = turb.manipulator.GetTransform()
+        point_pos = Tee[0:3,3]
+        point_direction = -Tee[0:3,0]
+        point_direction = point_direction/sqrt(dot(point_direction,
+                                                   point_direction))
+        point = concatenate((point_pos, point_direction))
+        disturbance = random.uniform(-0.01,0.01,3)
+        point[0:3] = point[0:3]+disturbance
+        res = planning.orientation_error_optimization(turb, point)
+        self.assertTrue(res.success, msg = 'No solution was found')
+
+    def test_compute_robot_joints(self):
+        """
+        The test generates a trajectory and computes robot joints.
+        """
+        turbconf = TurbineConfig.load(self.path, self.test_dir)
+        turb = Turbine(turbconf)
+        robot_pos = turb.robot.GetTransform()[0:3,3]
+
+        theta = arange(-pi/10,pi/10,0.001)
+        # Generating positions
+        x = -1 + (2 - cos(3*theta))*cos(theta)
+        y = 1 + (2 - cos(3*theta))*sin(theta)
+        z = ones(len(theta))*robot_pos[2]
+        positions = [[x[i],y[i],z[i]] for i in range(0,len(x))]
+
+        # Generating normal vectors
+        ny = (2*cos(theta)+cos(2*theta)-2*cos(4*theta))/(-2*sin(theta)+sin(2*theta)+2*sin(4*theta))
+        nx = ones(len(theta))
+        nz = zeros(len(theta))
+        normal_vectors = array([[nx[i],ny[i],nz[i]] for i in range(0,len(nx))])
+        normal_vectors = normal_vectors*(1.0/sqrt(sum(normal_vectors*normal_vectors,1))).reshape(len(normal_vectors),1)
+        
+        trajectory = [[positions[i][0], positions[i][1], positions[i][2],
+                       normal_vectors[i][0], normal_vectors[i][1], normal_vectors[i][2]] for i in range(0,len(positions))]
+        joint_solutions = planning.compute_robot_joints(turb, trajectory)
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
