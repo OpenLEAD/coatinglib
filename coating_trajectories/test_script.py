af6b1ab8aa0de6a41486b354ded2c10a5c1efae9
elael <elael2@gmail.com>
2016-10-17 14:05:58 -0200

    CHANGE test_script to fit DB generation

--- coating_trajectories/test_script.py
+++ coating_trajectories/test_script.py
@@ -11,22 +11,40 @@ import numpy as np
 import rail_place
 RP = rail_place.rand_rail(turb,20)
 rpt = rail_place.RailPlace((.80,0.15,0))
+import planning
+
+import blade_modeling
+folder = "jiraublade"
+blade = blade_modeling.BladeModeling(turb, turb.blades[0])
+
+#blade.load_trajectory(os.path.join(folder,"trajectory/trajectory.xml"))
+
+for trajectory in blade.trajectories:
+    vz.plot(trajectory)
 
 #position trajectories
-
+Ro = np.transpose(np.array([[-1,0,0],[0,0,1],[0,1,0]]))
+To = np.eye(4)
+To[0:3,0:3]=Ro
+x=None
 for rp in RP:
-
+    
     turb.place_rail(rp)
+    turb.place_robot(rp)
+    turb.robot.SetTransform(np.dot(turb.robot.GetTransform(),To))
+    
     if turb.check_rail_collision():
         continue
     
-    turb.place_robot(rp)
+
     if turb.check_robot_collision():
         continue
 
-    filtered_trajectories = filter_trajectories(turb,trajectories)
+    filtered_trajectories = path_filters.filter_trajectories(turb, blade.trajectories)
     
-# filter PCL
+    #x = input(x)
+
+    
 # follow parallels (verifing limits) and saving points
 # save railplace info to points
 
