d8918bab0cd96dbbf4e07570655e66b98fb44273
renan028 <renan028@gmail.com>
2016-12-23 17:30:08 -0200

    CREATE grid_validation method to recompute the trajectory and a tolerance check

--- coating_trajectories/main_grid_coating.py
+++ coating_trajectories/main_grid_coating.py
@@ -74,12 +74,69 @@ def load_blade(folder):
     blade.load_trajectory(xml_trajectories_path)
     return blade       
 
+def base_grid_validation(blade_angle, rays):
+    """
+    Given the real blade angle:
+    1) rotate the blades (update the environment);
+    2) rotate the RBF model of the blade;
+    3) rotate grid points;
+    4) organize trajectories (removing empty points, adding borders,
+    and making one full zigzagging list);
+    5) compute optimization.
+
+    Keyword arguments:
+    blade_angle -- real angle of the blade
+    rays -- points to be coated (list-n of lists-mx6) w.r.t. the 0 rotor angle.
+    """
+    
+    T = matrixFromAxisAngle([blade_angle,0,0])
+    for blade in turb.blades:
+        blade.SetTransform(T)
+        
+    blade = dict_angle_blade[0]
+    blade.rotate_models(T)
+
+    rotated_rays = deepcopy(rays)
+
+    rotated_rays = mathtools.rotate_trajectories(rotated_rays, T)
+    organized_rays = []
+    for i in range(0,len(rotated_rays)):
+        if i%2==0:
+            organized_rays += rotated_rays[i]
+        else:
+            organized_rays += reversed(rotated_rays[i])
+
+    turb.robot.GetLink('Flame').Enable(False)
+    joint_solutions = planning.compute_robot_joints(turb, organized_rays, 0,
+                                                    blade.trajectory_iter_surface)
+    score = len(joint_solutions)*1.0/len(organized_rays)
+    
+    return joint_solutions, score
+
+def tolerance_test(sorted_base, trajectories, borders):
+    score, base, angle = sorted_base
+    blade = dict_angle_blade[0]
+    rays = dict_angle_db[0].compute_rays_from_parallels(blade, trajectories, borders)
+
+    rp = rail_place.RailPlace(base)
+    turb.place_rail(rp)
+    turb.place_robot(rp)
+    
+    for ang in linspace(angle-5*pi/180,angle+5*pi/180,11):
+        x = raw_input('ang: '+str(ang*180/pi))
+        joint_solutions, score = base_grid_validation(ang, rays)
+        print 'score: ', score, '\n'
+    return
+
 if __name__ == '__main__':
 
+    #-----------------------------------------------------------------------
+    # DB inputs
     db_directories = ['db', 'db_45', 'db_-45']
     db_angles = [0, pi/4, -pi/4]
     blade_folder = ['jiraublade_hd_filtered', 'jiraublade_hd_filtered_45',
                     'jiraublade_hd_filtered_-45']
+    #-----------------------------------------------------------------------
     
     dir_test = join(realpath('.'),'test')
     os.environ['OPENRAVE_DATA'] = str(dir_test)
@@ -95,7 +152,8 @@ if __name__ == '__main__':
     
     vis = Visualizer(turb.env)
 
+    # Grid input
     grid_num = 0
-
-    bases, scores = base_for_grid_coating(grid_num)
-    plot_base_for_grid_coating(grid_num, bases, scores)
+    sorted_bases, trajectories, borders = base_for_grid_coating(grid_num)
+    #plot_base_for_grid_coating(sorted_bases, trajectories, borders)
+    tolerance_test(sorted_bases[0], trajectories, borders)
