30799d5bbe3291614abf6f22004b243361b76337
renan028 <renan028@gmail.com>
2017-03-14 17:04:05 -0300

    ADD method to compute minimal solution for secondary rail

--- coating_trajectories/blade_coverage.py
+++ coating_trajectories/blade_coverage.py
@@ -6,6 +6,7 @@ from numpy import array, tan, arange
 from math import pi as pi
 from os.path import join, realpath
 from os import environ
+import itertools
 
 def check_line(line, grids_num, grid_bases, line_grid, line_grid_dist):
     grid_dist = dict()
@@ -29,6 +30,39 @@ def compute_all_lines(a, b, grids_num, grid_bases):
                                               grid_bases, line_grid, line_grid_dist)
     return line_grid, line_grid_dist
 
+def set_union_bases(line_grid, lines):
+    line_union = set()
+    for line in lines:
+        line_union = line_union.union(line_grid[line])
+    return line_union
+
+def compute_minimal_lines(line_grid, grid_nums):
+    lines = line_grid.keys()
+    set_grid_nums = set(grid_nums)
+    k = len(set_grid_nums)
+    comb_sol = []
+    best_sol = []
+    stop=0
+    for i in range(1,len(lines)):
+        for line_comb in itertools.combinations(lines,i):
+            line_union = set_union_bases(line_grid, line_comb)
+            difference = set_grid_nums.difference(line_union)
+            n = len(difference)
+            if n==0:
+                stop=1
+            if n<=k:
+                if n<k:
+                    k = n
+                    best_sol = []
+                    best_sol.append(line_comb)
+                    print 'k=', k
+                    print 'best_sol=', best_sol
+                else:
+                    best_sol.append(line_comb)
+        if stop:
+            break
+    return best_sol          
+
 if __name__ == '__main__':
 
     dir_test = join(realpath('.'),'test')
@@ -50,4 +84,5 @@ if __name__ == '__main__':
     a = tan(array(range(-80,80,1))*pi/180)
     b = arange(0,cfg.environment.y_min,-0.1)
     line_grid, line_grid_dist = compute_all_lines(a, b, grid_nums, grid_bases)
+    best_sol = compute_minimal_lines(line_grid, grid_nums)
     
