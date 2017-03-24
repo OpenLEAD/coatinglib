4824f75f736cc3b2527191cf8158e4fe62138572
elael <elael2@gmail.com>
2017-03-24 14:39:48 -0300

    ADD button action para selecao de lado

--- coating_trajectories/main_interface.py
+++ coating_trajectories/main_interface.py
@@ -1,11 +1,32 @@
-from PyQt4 import QtGui  # Import the PyQt4 module we'll need
+from PyQt4 import QtCore, QtGui  # Import the PyQt4 module we'll need
 import sys  # We need sys so that we can pass argv to QApplication
+
+import blade_coverage
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 import qt_interface  # This file holds our MainWindow and all design related things
 
 # it also keeps events etc that we defined in Qt Designer
 import os  # For listing directory methods
 
+dics = {
+    "Faces": ['jusante','montante',blade_coverage.jusante()+blade_coverage.montante()],
+    "Lados": ['borda_direita','borda_esquerda',blade_coverage.lip()],
+    "Lips": ['lip','lip',blade_coverage.lip()],
+    }
 
 class ExampleApp(QtGui.QMainWindow, qt_interface.Ui_MainWindow):
     def __init__(self):
@@ -16,8 +37,17 @@ class ExampleApp(QtGui.QMainWindow, qt_interface.Ui_MainWindow):
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
         # It sets up layout and widgets that are defined
-        self.actionOpen.triggered.connect(self.browse_folder)  # When the button is pressed
-        self.jusantepng.setFixedSize(640,480)
+        self.actionOpen.triggered.connect(self.browse_folder)  # When the button is presse
+        self.dbs.addItems(dics.keys())
+        self.dbs.currentIndexChanged[QtCore.QString].connect(self.apply_figs)
+        
+    def apply_figs(self,key):
+        self.figesq.setPixmap(QtGui.QPixmap(_fromUtf8("interface_figs/"+dics[str(key)][0]+".png")))
+        self.figdir.setPixmap(QtGui.QPixmap(_fromUtf8("interface_figs/"+dics[str(key)][1]+".png")))
+        self.grid.clear()
+        self.grid.addItems(map(lambda s: str(s),dics[str(key)][2]))
+        
+        
          
     def browse_folder(self):
         directory = QtGui.QFileDialog.getOpenFileName(self,
