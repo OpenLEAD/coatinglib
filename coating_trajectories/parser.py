115ac9cc37c745ca0bc38b5db6df0cf963dfacc1
renan028 <renan028@gmail.com>
2017-03-31 15:27:30 -0300

    MODIFY parser to use subparsers

--- coating_trajectories/parser.py
+++ coating_trajectories/parser.py
@@ -11,20 +11,28 @@ grids_available = {'jusante':blade_coverage.jusante_grids,
                    'lip': blade_coverage.lip_grids
                   }
 
-parser = argparse.ArgumentParser(description='Coating script.')
-group = parser.add_mutually_exclusive_group()
+def coverage(args):
+    db_directories, grid_path = area_db[args.Area]
+    print blade_coverage.robot_base_position(db_directories, grid_path, args.Grid)
 
-# Functions
-group.add_argument('-p', '--position', nargs=2, metavar=('Area', 'Grid'),
-                    help='Compute the rail and robot base position. Area = '+str(area_db.keys())+'. Grid = int.')
-group.add_argument('-g', '--grids', nargs=1, metavar=('Area'), 
-                    help='Return available grids given area . Area = '+str(area_db.keys())+'.')
+def grids_out(args):
+    print grids_available[args.Area]()
+
+parser = argparse.ArgumentParser(description='Coating script.')
+parser.add_argument('Area', choices=area_db.keys(), type=str,
+                    help = 'Area to be coated.')
+
+subparsers = parser.add_subparsers(help='Functions')
+
+# Function to return PSAlpha
+parser_p = subparsers.add_parser('p', help='Compute the rail and robot base position.')
+parser_p.add_argument('Grid', type=int, help='Grid to be coated.')
+parser_p.set_defaults(func=coverage)
+
+# Function to return Grids
+parser_g = subparsers.add_parser('g', help='Return available grids given area.')
+parser_g.set_defaults(func=grids_out)
 
 
 args = parser.parse_args()
-
-if args.position:
-    db_directories, grid_path = area_db[args.position[0]]
-    print blade_coverage.robot_base_position(db_directories, grid_path, int(args.position[1]))
-elif args.grids:
-    print grids_available[args.grids[0]]()
+args.func(args)
