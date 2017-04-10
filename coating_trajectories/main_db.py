ddd52710699235f85dc4c706e8292881732d4f75
renan028 <renan028@gmail.com>
2017-04-10 17:25:46 -0300

    MODIFY several methods to meet the new organization. Include, also, function from main_grid_coating

--- coating_trajectories/main_db.py
+++ coating_trajectories/main_db.py
@@ -27,18 +27,8 @@ def generate_db_joints():
     """
     
     turb.robot.GetLink('Flame').Enable(False)
-    blade = load_blade(blade_folder)
-    DB = db.DB(directory)
-
-    path = join(directory,'not_merged')
     path_joints = join(path,'joints')
     path_seg = join(path,'seg')
-
-    try:
-        makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
 
     try:
         makedirs(path_joints)
@@ -53,46 +43,31 @@ def generate_db_joints():
             raise
     
     while True:
-        with open(join(directory,'fixed_db','db_visited_bases.pkl'), 'rb') as f:
-            db_visited_bases = cPickle.load(f)
+        visited_bases = db.load_pickle(join(path,'visited_bases.pkl'))
         base_num = None
-        for key, value in db_visited_bases.iteritems():
+        for key, value in visited_bases.iteritems():
             if value == False:
-                db_visited_bases[key] = True
+                visited_bases[key] = True
                 base_num = key
                 break
         if base_num is None:
             break
-        with open(join(directory,'fixed_db','db_visited_bases.pkl'), 'wb') as f:
-            cPickle.dump(db_visited_bases, f, cPickle.HIGHEST_PROTOCOL)
-        del db_visited_bases
+        db.save_pickle(visited_bases,join(path,'visited_bases.pkl'))
+        del visited_bases
 
-        db_bases_to_num = DB.load_db_bases_to_num()
-        for key, value in db_bases_to_num.iteritems():
-            if value == base_num:
-                base = key
-                break
-        del db_bases_to_num
-
-        rp = rail_place.RailPlace(base)
-        turb.place_rail(rp)
-        turb.place_robot(rp)
-
-        if turb.check_rail_collision():
+        base_to_joints, base_to_seg = DB.generate_db_joints(base_num)
+        if base_to_joints == None:
             continue
-        if turb.check_robotbase_collision():
-            continue
-
-        db_base_to_joints, db_base_to_seg = DB.generate_db_joints(turb, blade, base_num)
+        
         print 'saving base_num: ', base_num
 
         try:
-            DB.save_db_pickle(db_base_to_joints, join(path_joints,str(base_num)+'.pkl'))
+            db.save_pickle(base_to_joints, join(path_joints,str(base_num)+'.pkl'))
         except IOError:
             raise 'Error saving db_base_to_joints.pkl'
         
         try:
-            DB.save_db_pickle(db_base_to_seg, join(path_seg,str(base_num)+'.pkl'))
+            db.save_pickle(base_to_seg, join(path_seg,str(base_num)+'.pkl'))
         except IOError:
             raise 'Error saving db_base_to_seg.pkl'
     return
@@ -102,44 +77,31 @@ def generate_robot_positions(number_of_positions=1000):
     Function to generate random positions for the base of the robot.
     The positions will be saved and a db_visited_bases will be saved (False to everything).
     """
+
     rp = rail_place.rand_rail(turb.config, number_of_positions)
     keys = [ tuple(p.getPSAlpha()) for p in rp]
     
     psalpha_dict = dict(zip(keys, range(0,len(keys))))
     visited_bases = dict(zip(range(0,len(keys)), zeros(len(keys), dtype=bool)))
 
-    path = join(directory,'fixed_db')
-    
-    try:
-        makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
-        
-    with open(join(path,'db_bases_to_num.pkl'), 'wb') as f:
-        cPickle.dump(psalpha_dict, f, cPickle.HIGHEST_PROTOCOL)
-    with open(join(path,'db_visited_bases.pkl'), 'wb') as f:
-        cPickle.dump(visited_bases, f, cPickle.HIGHEST_PROTOCOL)
+    db.save_pickle(psalpha_dict,join(path,'bases_to_num.pkl'))
+    db.save_pickle(visited_bases,join(path,'visited_bases.pkl'))
     return       
 
 def create_db_with_blade():
     """
     Funciton uses the blade points to create an empty db.
     """
-    
-    blade = load_blade(blade_folder)
-    DB = db.DB(directory, blade)
-    del blade
+    DB.create_points_to_num()
+    DB.create_db()
     return
 
 def save_meridians(meridians):
-    with open(join(directory,'fixed_db','meridians.pkl'), 'wb') as f:
-        cPickle.dump(meridians, f, cPickle.HIGHEST_PROTOCOL)
+    db.save_pickle(meridians,join(area,'meridians.pkl'))
     return
 
 def save_parallels(parallels):
-    with open(join(directory,'fixed_db','parallels.pkl'), 'wb') as f:
-        cPickle.dump(parallels, f, cPickle.HIGHEST_PROTOCOL)
+    db.save_pickle(parallels,join(area,'parallels.pkl'))
     return
 
 def load_blade(folder):
@@ -155,11 +117,8 @@ def load_blade(folder):
 def make_grid(number_of_meridians, number_of_parallels, init_parallel):
     """
     See make_grid method in db.py.
-    """
-    
-    DB = db.DB(directory)
-    blade = load_blade(blade_folder_full)
-    return DB.make_grid(blade, number_of_meridians, number_of_parallels, init_parallel)
+    """   
+    return DB.make_grid(number_of_meridians, number_of_parallels, init_parallel)
 
 def blade_borders(meridians):
     """
@@ -186,10 +145,7 @@ def create_db_grid():
     After the grid creation, borders and analysis if it makes sense,
     a db_grid must be created. See create_db_grid in db.py.
     """
-    
-    blade = load_blade(blade_folder)
-    DB = db.DB(directory)
-    DB.create_db_grid(blade)
+    DB.create_db_grid()
     return
 
 def grid_pick():
@@ -197,45 +153,37 @@ def grid_pick():
     After the db_grid creation, some grids may not make sense.
     Iteractive remove those grids with this function.
     """
-    
-    blade = load_blade(blade_folder)
-    DB = db.DB(directory)
-    db_grid_to_mp = DB.load_db_grid_to_mp()
-    db_grid_to_mp_copy = deepcopy(db_grid_to_mp)
-    db_grid_to_bases = DB.load_db_grid_to_bases()
-    db_grid_to_trajectories = DB.load_db_grid_to_trajectories()
+
+    grid_to_mp = DB.load_grid_to_mp()
+    grid_to_mp_copy = deepcopy(db_grid_to_mp)
+    grid_to_trajectories = DB.load_grid_to_trajectories()
     meridians = DB.load_grid_meridian()
     parallels = DB.load_grid_parallel()
     
-    for key, value in db_grid_to_mp.iteritems():
-        rays = DB.compute_rays_from_parallels(blade, db_grid_to_trajectories[key][0],
-                                              db_grid_to_trajectories[key][1])
+    for key, value in grid_to_mp.iteritems():
+        rays = DB.compute_rays_from_parallels(grid_to_trajectories[key][0],
+                                              grid_to_trajectories[key][1])
         s = vis.plot(meridians[value[0][0]],'meridian')
         s = vis.plot(meridians[value[0][1]],'meridian')
         s = vis.plot(parallels[value[1][0]],'parallel')
         s = vis.plot(parallels[value[1][1]],'parallel')
         s = vis.plot_lists(rays,'points', color=(1,0,0))
-        print 'number of bases = ', len(db_grid_to_bases[key])
+        print 'number of bases = ', len(grid_to_bases[key])
         x = raw_input('Remove base ? (y,n)')
         if x=='y':
-            db_grid_to_mp_copy.pop(key, None)
-            db_grid_to_bases.pop(key, None)
-            db_grid_to_trajectories.pop(key, None)
+            grid_to_mp_copy.pop(key, None)
+            grid_to_trajectories.pop(key, None)
             print "Grid removed"
         vis.remove_points('meridian')
         vis.remove_points('parallel')
         vis.remove_points('points')
 
         try:
-            DB.save_db_pickle(db_grid_to_mp_copy, join(DB.path,'fixed_db','db_grid_to_mp.pkl'))
+            db.save_pickle(grid_to_mp_copy, join(DB.path,'grid_to_mp.pkl'))
         except IOError: None
 
         try:
-            DB.save_db_pickle(db_grid_to_bases, join(DB.path,'fixed_db','db_grid_to_bases.pkl'))
-        except IOError: None
-
-        try:
-            DB.save_db_pickle(db_grid_to_trajectories, join(DB.path,'fixed_db','db_grid_to_trajectories.pkl'))
+            db.save_pickle(grid_to_trajectories, join(DB.path,'grid_to_trajectories.pkl'))
         except IOError: None
 
     return
@@ -245,12 +193,8 @@ def grid_add():
     After db_grid creation, some grids may be added.
     """
     
-    blade = load_blade(blade_folder)
-    DB = db.DB(directory)
-    
-    db_grid_to_mp = DB.load_db_grid_to_mp()
-    db_grid_to_bases = DB.load_db_grid_to_bases()
-    db_grid_to_trajectories = DB.load_db_grid_to_trajectories()
+    grid_to_mp = DB.load_grid_to_mp()
+    grid_to_trajectories = DB.load_grid_to_trajectories()
     meridians = DB.load_grid_meridian()
     parallels = DB.load_grid_parallel()
     
@@ -259,46 +203,41 @@ def grid_add():
     s = vis.plot(parallels[0],'parallels',color=(0,1,0))
     s = vis.plot_lists(parallels,'parallels')
     
-    for key, value in db_grid_to_mp.iteritems():
-        rays = DB.compute_rays_from_parallels(blade, db_grid_to_trajectories[key][0],
-                                              db_grid_to_trajectories[key][1])
+    for key, value in grid_to_mp.iteritems():
+        rays = DB.compute_rays_from_parallels(grid_to_trajectories[key][0],
+                                              grid_to_trajectories[key][1])
         s = vis.plot_lists(rays,'points', color=tuple(random.rand(3)))
 
     while True:
-        db_grid_to_mp = DB.load_db_grid_to_mp()
-        db_grid_to_bases = DB.load_db_grid_to_bases()
-        db_grid_to_trajectories = DB.load_db_grid_to_trajectories()
+        grid_to_mp = DB.load_grid_to_mp()
+        grid_to_trajectories = DB.load_grid_to_trajectories()
         
         x = raw_input('Add: [(m1,m2),(p1,p2)]')
         grid = ast.literal_eval(x)
-        key = max(db_grid_to_mp.keys())+1
+        key = max(grid_to_mp.keys())+1
         trajectories_in_grid, border = DB.get_points_in_grid(
-            blade,[meridians[grid[0][0]],meridians[grid[0][1]]],
+            [meridians[grid[0][0]],meridians[grid[0][1]]],
             [parallels[grid[1][0]],parallels[grid[1][1]]])
-        rays = DB.compute_rays_from_parallels(blade, trajectories_in_grid,border)
+        rays = DB.compute_rays_from_parallels(trajectories_in_grid,border)
         vis.plot_lists(rays,'rays')
         x = raw_input('Save ? (y,n)')
         if x == 'y':
             bases = DB.get_bases_trajectories(trajectories_in_grid)
             db_grid_to_mp[key] = grid
-            db_grid_to_bases[key] = bases 
             db_grid_to_trajectories[key] = [trajectories_in_grid, border]
             try:
-                DB.save_db_pickle(db_grid_to_mp, join(DB.path,'fixed_db','db_grid_to_mp.pkl'))
+                db.save_pickle(grid_to_mp, join(DB.path,'grid_to_mp.pkl'))
             except IOError: None
             try:
-                DB.save_db_pickle(db_grid_to_bases, join(DB.path,'fixed_db','db_grid_to_bases.pkl'))
-            except IOError: None
-            try:
-                DB.save_db_pickle(db_grid_to_trajectories, join(DB.path,'fixed_db','db_grid_to_trajectories.pkl'))
+                db.save_pickle(grid_to_trajectories, join(DB.path,'grid_to_trajectories.pkl'))
             except IOError: None
 
 def compute_points_to_remove(grid_num):
+
     blade = load_blade(blade_folder)
-    DB = db.DB(directory)
-    db_grid_to_trajectories = DB.load_db_grid_to_trajectories()
-    trajectories, borders = db_grid_to_trajectories[grid_num]
-    rays = DB.compute_rays_from_parallels(blade, trajectories, borders)
+    grid_to_trajectories = DB.load_grid_to_trajectories()
+    trajectories, borders = grid_to_trajectories[grid_num]
+    rays = DB.compute_rays_from_parallels(trajectories, borders)
     rays_traj = DB.compute_rays_from_parallels(blade, trajectories)
 
     def get_ray(point):
@@ -333,43 +272,85 @@ def compute_points_to_remove(grid_num):
     return borders, points_to_remove
 
 def remove_points_from_db(grid_num, new_border, points_to_remove):
-    blade = load_blade(blade_folder)
-    DB = db.DB(directory)
-    db_grid_to_trajectories = DB.load_db_grid_to_trajectories()
-    trajectories, _ = db_grid_to_trajectories[grid_num]
-    db_grid_to_trajectories[grid_num] = [trajectories, new_border]
+
+    grid_to_trajectories = DB.load_grid_to_trajectories()
+    trajectories, _ = grid_to_trajectories[grid_num]
+    grid_to_trajectories[grid_num] = [trajectories, new_border]
     try:
-        DB.save_db_pickle(db_grid_to_trajectories, join(DB.path,'fixed_db','db_grid_to_trajectories.pkl'))
+        db.save_pickle(db_grid_to_trajectories, join(DB.path,'grid_to_trajectories.pkl'))
     except IOError: None
     DB.remove_point(points_to_remove)
     return 
 
 def clear_db_visited_bases():
-    DB = db.DB(directory)
-    DB.clear_db_visited_bases()
+    DB.clear_visited_bases()
     return
 
 def create_db_from_segments(path):
-    DB = db.DB(directory)
     db_main = DB.create_db_from_segments(path)
-    DB.save_db_pickle(db_main, join(path,'db.pkl') )
+    db.save_pickle(db_main, join(path,'db.pkl') )
     return
 
-def turbine_rotate(turb, T):
+def turbine_rotate(turb):
     for blade in turb.blades:
-        blade.SetTransform(T)
+        blade.SetTransform(DB.T)
     return
+
+def base_grid_coating(grid_num):
+    
+    grid_to_trajectories = DB.load_grid_to_trajectories()
+    trajectories, borders = grid_to_trajectories[grid_num]
+    bases, scores = DB.get_best_bases_trajectories(trajectories)
+    sorted_bases = sorted(zip(-array(scores),bases))
+    return sorted_bases
+
+def bases_grids_coating(threshold):
+    
+    grid_to_trajectories = DB.load_grid_to_trajectories()
+    grid_bases = dict()
+    for grid_num in grid_to_trajectories.keys():
+        trajectories, borders = grid_to_trajectories[grid_num]
+        sorted_bases = base_grid_coating(grid_num)
+        xy = []
+        for sorted_base in sorted_bases:
+            score, base = sorted_base
+            if -score >= threshold:
+                rp = rail_place.RailPlace(base)
+                xyz = rp.getXYZ(cfg)
+                value = set([(xyz[0],xyz[1])])
+                grid_bases[grid_num] = grid_bases.get(grid_num,set()) | value
+            else: break
+    db.save_pickle(grid_bases,join(DB.db_main_path,'grid_bases.pkl'))
+    return
+
+def verify_base_grid_threshold():
+    grid_to_trajectories = DB.load_grid_to_trajectories()
+    grid_bases = dict()
+    for grid_num in grid_to_trajectories.keys():
+        trajectories, borders = grid_to_trajectories[grid_num]
+        sorted_bases = base_grid_coating(grid_num)
+        xy = []
+        sorted_base = sorted_bases[0]
+        score, base = sorted_base
+        print 'Grid: ', grid_num, '/ score: ', -score
+    return
+
     
 if __name__ == '__main__':
 
-    path = 'FACE'
+    area, db_name = 'FACE', 'db'
+    path = join(area,db_name)
+    
     dir_test = join(realpath('.'),'test')
     os.environ['OPENRAVE_DATA'] = str(dir_test)
     cfg = TurbineConfig.load('turbine_unittest.cfg','test')
     turb = Turbine(cfg)
-    DB = db.DB(path, turb)
-    turbine_rotate(turb, DB.T)
-    vis = Visualizer(turb.env)
+    
+    DB = db.DB(area, turb, db_name)
+    turbine_rotate(turb)
+    
+    threshold = 0.92
+    #vis = Visualizer(turb.env)
     
     #generate_robot_positions()
     #create_db_with_blade()
@@ -391,3 +372,4 @@ if __name__ == '__main__':
     #remove_points_from_db(24, borders, points_to_remove)
 
     #create_db_from_segments(new_segs_path)
+    bases_grids_coating(threshold)
