00e4363d7ca2c043889e4ebcc04f3eeca5cc8e4c
renan028 <renan028@gmail.com>
2017-04-20 15:30:03 -0300

    ADD functions for new db integration

--- coating_trajectories/parser.py
+++ coating_trajectories/parser.py
@@ -1,9 +1,18 @@
 import blade_coverage
 import argparse
+import db
+from os import environ
 
-area_db = {'jusante':['db','jusante'],
-           'montante': ['db','montante'],
-           'lip': ['db_lip','lip']
+
+dir_test = join(realpath('.'),'test')
+environ['OPENRAVE_DATA'] = str(dir_test)
+cfg = TurbineConfig.load('turbine_unittest.cfg','test')
+turb = Turbine(cfg)
+
+counter = 0 
+area_db = {'jusante':['FACE'],
+           'montante': ['FACE'],
+           'lip': ['LIP']
           }
 
 grids_available = {'jusante':blade_coverage.jusante_grids,
@@ -12,8 +21,11 @@ grids_available = {'jusante':blade_coverage.jusante_grids,
                   }
 
 def coverage(args):
-    db_directories, grid_path = area_db[args.Area]
-    print blade_coverage.robot_base_position(db_directories, grid_path, args.Grid)
+    global counter
+    DB = db.DB(area_db[args.Area],turb)
+    psa = DB.get_rail_configuration_n(grids_to_coat, 'sum', counter)
+    counter+=1
+    print psa
 
 def grids_out(args):
     print grids_available[args.Area]()
@@ -25,7 +37,7 @@ subparsers = parser.add_subparsers(help='Functions')
 # Function to return PSAlpha
 parser_p = subparsers.add_parser('position', help='Compute the rail and robot base position.')
 parser_p.add_argument('Area', choices=area_db.keys(), type=str, help='Area to be coated.')
-parser_p.add_argument('Grid', type=int, help='Grid to be coated.')
+parser_p.add_argument('Grids', nargs='+', type=int, help='Grids to be coated.')
 parser_p.set_defaults(func=coverage)
 
 # Function to return Grids
