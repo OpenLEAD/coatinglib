0eb9cb1785a0ced5dde2eca0d860cd77a563b06b
renan028 <renan028@gmail.com>
2017-04-26 15:58:26 -0300

    ADD validation to parser

--- coating_trajectories/parser.py
+++ coating_trajectories/parser.py
@@ -1,8 +1,3 @@
-import sys
-import io
-
-save_stdout = sys.stdout
-sys.stdout = io.BytesIO()
 import blade_coverage
 import argparse
 import db
@@ -11,7 +6,6 @@ from os.path import realpath, join
 from turbine_config import TurbineConfig
 from turbine import Turbine
 
-
 dir_test = join(environ['PYTHON_COAT'],'test')
 environ['OPENRAVE_DATA'] = str(dir_test)
 cfg = TurbineConfig.load('turbine_unittest.cfg',dir_test)
@@ -19,21 +13,32 @@ turb = Turbine(cfg)
 
 area_db = {'jusante':'FACE',
            'montante': 'FACE',
-           'lip': 'LIP'
+           'lip': 'LIP',
+           'border':'BORDER'
           }
 
 grids_available = {'jusante':blade_coverage.jusante_grids,
                    'montante': blade_coverage.montante_grids,
-                   'lip': blade_coverage.lip_grids
+                   'lip': blade_coverage.lip_grids,
+                   'border': blade_coverage.border_grids
                   }
+
 
 def coverage(args):
     DB = db.DB(area_db[args.Area],turb)
-    psa = DB.get_rail_configuration_n(args.Grids, 'sum', 0)
+    psa, fcomp = DB.get_rail_configuration_n(args.Grids, 'sum', args.ans)
     print psa
+    return
 
 def grids_out(args):
     print grids_available[args.Area]()
+    return
+
+def validate(args):
+    DB = db.DB(area_db[args.Area],turb)
+    score, joints = blade_coverage.base_grid_validation(DB, args.Grid)
+    print score
+    return 
 
 parser = argparse.ArgumentParser(description='Coating script.')
 
@@ -43,6 +48,7 @@ subparsers = parser.add_subparsers(help='Functions')
 parser_p = subparsers.add_parser('position', help='Compute the rail and robot base position.')
 parser_p.add_argument('Area', choices=area_db.keys(), type=str, help='Area to be coated.')
 parser_p.add_argument('Grids', nargs='+', type=int, help='Grids to be coated.')
+parser_p.add_argument('-ans', type=int, help='Answer number.', default=0)
 parser_p.set_defaults(func=coverage)
 
 # Function to return Grids
@@ -50,7 +56,11 @@ parser_g = subparsers.add_parser('grids', help='Return available grids given are
 parser_g.add_argument('Area', choices=area_db.keys(), type=str, help='Area to be coated.')
 parser_g.set_defaults(func=grids_out)
 
+# Function to return Grids
+parser_j = subparsers.add_parser('joints', help='Return joints for grid coating.')
+parser_j.add_argument('Area', choices=area_db.keys(), type=str, help='Area to be coated.')
+parser_j.add_argument('Grid', type=int, help='Grid to be coated.')
+parser_j.set_defaults(func=validate)
 
 args = parser.parse_args()
-sys.stdout = save_stdout
 args.func(args)
