2892f7da550633fe340b5436d3bb1df5eecc1589
renan028 <renan028@gmail.com>
2017-04-26 15:56:58 -0300

    ADD validation function and border grids

--- coating_trajectories/blade_coverage.py
+++ coating_trajectories/blade_coverage.py
@@ -1,19 +1,40 @@
-import cPickle
-from turbine import Turbine
-from turbine_config import TurbineConfig, ConfigFileError
-import mathtools
-from numpy import array, tan, arange, zeros, linalg, sign
-from math import pi as pi
-from os.path import join, realpath
-from os import environ, makedirs
-from visualizer import Visualizer
+from numpy import array
 import db
-import blade_modeling
-import errno
-import rail_place
-from math import atan2
-from itertools import combinations
+import planning
 
+def base_grid_validation(DB, grid):
+    """
+    Given the real blade angle:
+    1) rotate the blades (update the environment);
+    2) rotate the RBF model of the blade;
+    3) rotate grid points;
+    4) organize trajectories (removing empty points, adding borders,
+    and making one full zigzagging list);
+    5) compute optimization.
+
+    Keyword arguments:
+    DB -- AREA DATABASE
+    grid -- int
+    """
+
+    DB.T = DB.turb.blades[0].GetTransform()
+    parallels, borders = (DB.load_grid_to_trajectories())[grid]
+    rays = DB.compute_rays_from_parallels(parallels, borders)
+    blade = DB.load_blade()
+
+    organized_rays = []
+    for i in range(0,len(rays)):
+        if i%2==0:
+            organized_rays += rays[i]
+        else:
+            organized_rays += reversed(rays[i])
+    organized_rays = [x for x in organized_rays if x != []]
+
+    DB.turb.robot.GetLink('Flame').Enable(False)
+    joint_solutions = planning.compute_robot_joints_opt(DB.turb, organized_rays, 0,
+                                                    blade.trajectory_iter_surface)
+    score = len(joint_solutions)*1.0/len(organized_rays)
+    return score, joint_solutions
 
 def remove_nonstr_lines(line_grid, line_grid_dist, threshold_str):
     for key in line_grid.keys():
@@ -66,4 +87,10 @@ def montante_grids():
     return grid_nums
 
 def lip_grids():
-    return [0,1,2]      
+    return [0,1,2]
+
+def border_grids():
+    grid_nums = range(60,65)
+    grid_nums.extend(range(25,29))
+    grid_nums.extend([85])
+    return grid_nums
