fec0172937324a3ee3fc8c3fb2a61ef718a161b1
renan028 <renan028@gmail.com>
2017-04-26 15:58:54 -0300

    REMOVE imports

--- coating_trajectories/simple_shell.py
+++ coating_trajectories/simple_shell.py
@@ -7,8 +7,6 @@ dir_test = join(realpath('.'),'test')
 os.environ['OPENRAVE_DATA'] = str(dir_test)
 cfg = TurbineConfig.load('turbine_unittest.cfg','test')
 turb = Turbine(cfg)
-##import db
-##bases = db.get_bases('db_bases_to_num.pkl')
 
 import visualizer
 vis = visualizer.Visualizer(turb.env)
