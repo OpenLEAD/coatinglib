4e970b27e405ebc2ad9925b88d25ed0189cd0e2d
renan028 <renan028@gmail.com>
2017-04-28 17:00:16 -0300

    ADD functions for interpolation

--- coating_trajectories/blade_coverage.py
+++ coating_trajectories/blade_coverage.py
@@ -1,6 +1,22 @@
 from numpy import array
 import db
 import planning
+from openravepy import ConfigurationSpecification
+import mathtools
+
+def organize_rays(DB, grid):
+    DB.T = DB.turb.blades[0].GetTransform()
+    parallels, borders = (DB.load_grid_to_trajectories())[grid]
+    rays = DB.compute_rays_from_parallels(parallels, borders)
+
+    organized_rays = []
+    for i in range(0,len(rays)):
+        if i%2==0:
+            organized_rays += rays[i]
+        else:
+            organized_rays += reversed(rays[i])
+    organized_rays = [x for x in organized_rays if x != []]
+    return organized_rays
 
 def base_grid_validation(DB, grid):
     """
@@ -17,24 +33,39 @@ def base_grid_validation(DB, grid):
     grid -- int
     """
 
-    DB.T = DB.turb.blades[0].GetTransform()
-    parallels, borders = (DB.load_grid_to_trajectories())[grid]
-    rays = DB.compute_rays_from_parallels(parallels, borders)
+    organized_rays = organize_rays(DB, grid)
     blade = DB.load_blade()
-
-    organized_rays = []
-    for i in range(0,len(rays)):
-        if i%2==0:
-            organized_rays += rays[i]
-        else:
-            organized_rays += reversed(rays[i])
-    organized_rays = [x for x in organized_rays if x != []]
-
-    DB.turb.robot.GetLink('Flame').Enable(False)
     joint_solutions = planning.compute_robot_joints_opt(DB.turb, organized_rays, 0,
                                                     blade.trajectory_iter_surface)
     score = len(joint_solutions)*1.0/len(organized_rays)
+
     return score, joint_solutions
+
+def generate_linear_interpolation_joints(joint_solutions):
+    new_joints = []
+    new_joints.append(joint_solutions[0])
+    for i in range(0,len(joint_solutions)-1):
+        joints = mathtools.linear_interpolation_joint(joint_solutions[i], joint_solutions[i+1])
+        new_joints.extend(joints[1:])
+    return new_joints
+
+def generate_linear_interpolation_rays(organized_rays):
+    new_rays = []
+    new_rays.append(organized_rays[0])
+    for i in range(0,len(organized_rays)-1):
+        points = mathtools.linear_interpolation_ray(organized_rays[i], organized_rays[i+1])
+        new_rays.extend(points[1:])
+    return new_rays
+
+def trajectory_generation(DB, grid):
+    organized_rays = organize_rays(DB, grid)
+    new_rays = generate_linear_interpolation_rays(organized_rays)
+    blade = DB.load_blade()
+    joint_solutions = planning.compute_robot_joints(
+        DB.turb, new_rays, 0, blade.trajectory_iter_surface)
+    customspec = ConfigurationSpecification()
+    customspec.AddGroup('joint_values',6,'linear')
+    return joint_solutions
 
 def jusante_grids():
     grid_nums = range(0,15)
