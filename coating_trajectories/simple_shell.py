235dc7650c8bd5a009ec21fb683873731e030a8c
renan028 <renan028@gmail.com>
2017-05-03 17:16:47 -0300

    ADD simpleshell call movetohand

--- coating_trajectories/simple_shell.py
+++ coating_trajectories/simple_shell.py
@@ -15,9 +15,12 @@ import blade_coverage
 import db
 import planning
 from openravepy import interfaces, IkParameterization, RaveCreateTrajectory
+manip = turb.robot.GetActiveManipulator()
+basemanip = interfaces.BaseManipulation(turb.robot)
 
-turb.robot.GetLink('Flame').Enable(False)
-DB = db.DB('FACE',turb)
-joint_solutions = blade_coverage.trajectory_generation(DB, 7)
+#turb.robot.GetLink('Flame').Enable(False)
+#DB = db.DB('FACE',turb)
+#score, joint_solutions = blade_coverage.base_grid_validation(DB, 7)
+#res = blade_coverage.movetohandposition(DB.turb.robot,joint_solutions)
 
 
