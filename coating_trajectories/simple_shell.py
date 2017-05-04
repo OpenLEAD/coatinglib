3be02e364588eefdf2aecf87a1e566d7ee0be7db
renan028 <renan028@gmail.com>
2017-05-04 15:01:36 -0300

    ADD simple shell call

--- coating_trajectories/simple_shell.py
+++ coating_trajectories/simple_shell.py
@@ -15,12 +15,25 @@ import blade_coverage
 import db
 import planning
 from openravepy import interfaces, IkParameterization, RaveCreateTrajectory
-#manip = turb.robot.GetActiveManipulator()
+manip = turb.robot.GetActiveManipulator()
 #basemanip = interfaces.BaseManipulation(turb.robot)
 
 turb.robot.GetLink('Flame').Enable(False)
 DB = db.DB('FACE',turb)
 score, joint_solutions = blade_coverage.base_grid_validation(DB, 7)
-res = blade_coverage.movetohandposition(DB.turb.robot,joint_solutions)
+traj = blade_coverage.movetohandposition(DB.turb.robot,joint_solutions)
+#joints = []
+#for i in range(0,traj.GetNumWaypoints()):
+#    joints.append(traj.GetWaypoint(i)[0:6])
+#points = []
+#for joint in joints:
+#    turb.robot.SetDOFValues(joint)
+#    points.append(manip.GetTransform()[0:3,3])
+#p = vis.plot(points,'p',(1,0,0))
+
+for listT in traj:
+    for T in listT:
+        p = vis.plot(T[0:3,3],'p',(1,0,0))
+        p = vis.plot_normal(list(T[0:3,3])+list(T[0:3,0]),'p',(0,0,1))
 
 
