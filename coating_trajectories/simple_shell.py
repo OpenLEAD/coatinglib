f0b872c93c6c9230aaaf29e5e20f8abed854c747
renan028 <renan028@gmail.com>
2017-05-18 13:15:29 -0300

    REMOVE unused functions

--- coating_trajectories/simple_shell.py
+++ coating_trajectories/simple_shell.py
@@ -29,7 +29,7 @@ T = array([[ 1.,  0.,  0.,  0.],
        [ 0., -1.,  0.,  0.],
        [ 0.,  0.,  0.,  1.]])
 
-psa = (1.9000000000000035, 1.1917548675411605, 0.95993108859688125)
+psa = (1.2000000000000028, 1.2154441840549897, 0.4764012244017013)
 
 for blade in turb.blades:
     blade.SetTransform(T)
@@ -38,22 +38,9 @@ rp = rail_place.RailPlace(psa)
 turb.place_rail(rp)
 turb.place_robot(rp)
 
-
-
-##score, joint_solutions_list = blade_coverage.base_grid_validation_parallel(DB, 7)
-##traj = blade_coverage.movetohandposition_parallels(DB.turb.robot,joint_solutions)
-##joints = []
-##for i in range(0,traj.GetNumWaypoints()):
-##    joints.append(traj.GetWaypoint(i)[7:7+6])
-##points = []
-##for joint in joints:
-##    turb.robot.SetDOFValues(joint)
-##    points.append(manip.GetTransform()[0:3,3])
-##p = vis.plot(points,'p',(1,0,0))
-
-organized_rays = blade_coverage.organize_rays_in_parallels(DB, grid)
-joint_path = blade_coverage.move_dijkstra(turb, DB.load_blade(), organized_rays)
+organized_rays_list = blade_coverage.organize_rays_in_parallels(DB, grid)
+joint_path = blade_coverage.move_dijkstra(turb, DB.load_blade(), organized_rays_list, 3e-2)
 for path in joint_path:
 	for joint in path:
 		robot.SetDOFValues(joint)
-		time.sleep(0.01)
+		time.sleep(0.05)
