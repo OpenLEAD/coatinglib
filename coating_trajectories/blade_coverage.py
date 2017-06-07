79fc4dc5cf602bb2f0bc508260d1e1e8e23ab48e
renan028 <renan028@gmail.com>
2017-06-07 15:34:42 -0300

    ADD path class and complete validation

--- coating_trajectories/blade_coverage.py
+++ coating_trajectories/blade_coverage.py
@@ -1,19 +1,191 @@
-from numpy import array, linalg, dot, zeros, inf, vstack, mean, std
+from numpy import array, linalg, dot, zeros, inf, vstack, mean, std, cumsum
 from numpy import max as pymax
 from numpy import sum as pysum
 import db
 import planning
-from openravepy import ConfigurationSpecification, interfaces, planningutils
+from openravepy import ConfigurationSpecification, interfaces, planningutils, RaveCreateTrajectory
 import mathtools
 import time
+import errno
 from scipy.linalg import logm, expm
 from math import acos
+import rail_place
+from xml.etree import ElementTree as ET
+from os import listdir, makedirs
+from os.path import realpath, splitext, join
 
+class path:
+    """
+    Class to compute, store, and serialize joint path.
+    Robot's full trajectories are stored in OpenRave format Trajectory Class, as below:
+    
+    joint_values
+    joint_vel_calues
+    joint_acc_values
+    deltatime
+    """
+    
+    def __init__(self, rays=None):
+        """
+        Constructor fo class path.
+
+        @param rays (nx6 np.array) are the points and normal vectors of the points to coat (x,y,z,nx,ny,nz).
+        """
+        self.rays = rays
+        self.data = []
+        self.success = False
+
+    def execute(self, turbine, blade, threshold=5e-2):
+        """
+        Method to compute joint_values, joint_velocities, joint_accelerations and deltatimes.
+        It uses the Dijkstra planning algorithm (see move_dijkstra function).
+        
+        @param turbine is the turbine object.
+        @param blade is the blade object (you may use DB.load_blade(), for instance)
+        @param threshold (float) is the interpolation threshold, as rays are usually well spaced.
+        """
+        
+        if self.rays is None: return
+        
+        joint_path, rays_list = move_dijkstra(turbine, blade, self.rays, threshold)
+        if len(joint_path) == 0:
+            return
+        joint_path = refine_dijkstra(turbine, joint_path, rays_list, threshold)
+        
+        new_joint_path, new_joint_velocity_path, new_joint_acc_path, new_times_path = smooth_joint_MLS(turbine, joint_path)
+        maxAccel = turbine.robot.GetDOFMaxAccel()
+        maxVel = turbine.robot.GetDOFMaxVel()
+        vel = vstack(new_joint_velocity_path)
+        acc = vstack(new_joint_acc_path)
+        if (vel>maxVel).any(): return
+        if (acc>maxAccel).any(): return
+        self.success = True
+
+        ind = str()
+        for i in range(turbine.robot.GetDOF()): ind+=str(i)+' '
+
+        cs = ConfigurationSpecification()
+        _ = cs.AddGroup('joint_values '+turbine.robot.GetName()+' '+ind, len(turbine.robot.GetActiveDOFIndices()), 'cubic')
+        cs.AddDerivativeGroups(1,False)
+        cs.AddDerivativeGroups(2,False)
+        _ = cs.AddDeltaTimeGroup()
+
+        TRAJ = []
+        for i in range(len(new_joint_path)):
+            traj = RaveCreateTrajectory(turbine.env,'')
+            traj.Init(cs)
+            for j in range(len(new_joint_path[i])):
+                waypoint = list(new_joint_path[i][j])
+                waypoint.extend(list(new_joint_velocity_path[i][j]))
+                waypoint.extend(list(new_joint_acc_path[i][j]))
+                waypoint.extend([new_times_path[i][j]])
+                traj.Insert(traj.GetNumWaypoints(),waypoint)
+            TRAJ.append(traj)
+        self.data = TRAJ
+        return
+
+    def serialize(self, directory=''):
+        """
+        Method to serialize data in OpenRave format output is an xml file.
+        @param directory (str) is the relative path to the folder.
+        """
+        
+        try:
+            makedirs(directory)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+            
+        for i, traj in enumerate(self.data):
+            tree = ET.XML(traj.serialize())
+            with open(join(directory,'trajectory_'+str(i)+'.xml'), 'w') as f:
+                f.write(ET.tostring(tree))
+        return
+
+    def deserialize(self, turbine, directory):
+        """
+        Method to deserialize data in OpenRave format.
+        @param turbine is the turbine object.
+        @param directory (str) is the relative path to the folder.
+        """
+        
+        cs = ConfigurationSpecification()
+        _ = cs.AddGroup('joint_values', len(turbine.robot.GetActiveDOFIndices()), 'linear')
+        cs.AddDerivativeGroups(1,False)
+        cs.AddDerivativeGroups(2,False)
+        _ = cs.AddDeltaTimeGroup()
+        
+        path = os.realpath(path)
+        TRAJ = []
+        onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+        for afile in onlyfiles:
+            filename, file_extension = splitext(afile)
+            if file_extension == '.xml':
+                traj = RaveCreateTrajectory(turbine.env,'')
+                traj.Init(cs)
+                xml = ET.parse(open(join(directory,afile)))
+                traj.deserialize(ET.tostring(xml.getroot()))
+            TRAJ.append(traj)
+        self.data = TRAJ
+        return
+
+    def simulate(self, robot, parallel_number):
+        """
+        Method to simulate and visualize, in real time, the robot performing the coat.
+        @param robot is the robot object.
+        @param parallel_number (int) is parallel to be simulated.
+        """
+        
+        ans = robot.GetController().SetPath(path.data[parallel_number])
+
+    def get_joint(self, robot, parallel_number, point_number):
+        """
+        Method to get a specific joint_value from path.
+        @param robot is the robot object.
+        @param parallel_number (int) is the parallel number to get the joint.
+        @param point_number (int) is the specific point in the parallel to get the joint _value.
+        """
+        traj = path.data[parallel_number]
+        spec = traj.GetConfigurationSpecification()
+        return spec.ExtractJointValues(traj.GetWaypoint(point_number),robot,range(robot.GetDOF()))
+
+    def get_velocity(self, robot, parallel_number, point_number):
+        """
+        Method to get a specific joint_velocity from path.
+        @param robot is the robot object.
+        @param parallel_number (int) is the parallel number to get the joint_velocity.
+        @param point_number (int) is the specific point in the parallel to get the joint_velocity.
+        """
+        traj = path.data[parallel_number]
+        spec = traj.GetConfigurationSpecification()
+        return spec.ExtractJointValues(traj.GetWaypoint(point_number),robot,range(robot.GetDOF()),1)
+
+    def get_acc(self, robot, parallel_number, point_number):
+        """
+        Method to get a specific joint_acceleration from path.
+        @param robot is the robot object.
+        @param parallel_number (int) is the parallel number to get the joint_acceleration.
+        @param point_number (int) is the specific point in the parallel to get the joint_acceleration.
+        """
+        traj = path.data[parallel_number]
+        spec = traj.GetConfigurationSpecification()
+        return spec.ExtractJointValues(traj.GetWaypoint(point_number),robot,range(robot.GetDOF()),2)
+
+    def get_deltatime(self, robot, parallel_number, point_number):
+        """
+        Method to get a specific deltatime from path.
+        @param robot is the robot object.
+        @param parallel_number (int) is the parallel number to get the deltatime.
+        @param point_number (int) is the specific point in the parallel to get the deltatime.
+        """
+        traj = path.data[parallel_number]
+        spec = traj.GetConfigurationSpecification()
+        return spec.ExtractDeltaTime(traj.GetWaypoint(point_number))
+                                       
+        
 
 def organize_rays_in_parallels(DB, grid):
-    DB.T = DB.turb.blades[0].GetTransform()
-    parallels, borders = (DB.load_grid_to_trajectories())[grid]
-    rays = DB.compute_rays_from_parallels(parallels, borders)
+    rays = DB.compute_rays_from_grid(grid)
 
     organized_rays = []
     for i in range(0,len(rays)):
@@ -26,35 +198,38 @@ def organize_rays_in_parallels(DB, grid):
             organized_rays.append(list(reversed(not_empty_rays)))
     return organized_rays
 
-def base_grid_validation(DB, grid):
+def base_grid_validation(turbine, psa, DB, grid, threshold = 5e-2):
     """
     Given the real blade angle:
     1) rotate the blades (update the environment);
-    2) rotate the RBF model of the blade;
-    3) rotate grid points;
-    4) organize trajectories (removing empty points, adding borders,
-    and making one full zigzagging list);
-    5) compute optimization.
+    2) organize trajectories (removing empty points, adding borders,
+    and making zigzagging lists);
+    3) Dijkstra algorithm.
 
-
-def base_grid_validation_parallel(DB, grid):
-    """
-    Plan for each parallel.
-
-    Keyword arguments:
-    DB -- AREA DATABASE
-    grid -- int
+    @param turbine is the turbine object.
+    @param psa (tuple 1x3) primary, secondary, alpha (base position)
+    @param DB database object.
+    @param grid (int) grid to be coated.
+    @param threshold (float) is the interpolation threshold, as rays are usually well spaced.
     """
     
-    joint_solutions_list = []
-    organized_rays = organize_rays_in_parallels(DB, grid)
-    blade = DB.load_blade()
-    for rays in organized_rays:
-        joint_solutions_list.append(planning.compute_robot_joints(
-            DB.turb, rays, 0, blade.trajectory_iter_surface))
-    score = mathtools.lenlist(joint_solutions_list)*1.0/mathtools.lenlist(organized_rays)
-    return score, joint_solutions_list
+    robot = turbine.robot
+    manip = robot.GetActiveManipulator()
+    
+    T = DB.T
+    T = dot(T,linalg.inv(turbine.blades[3].GetTransform())) #HARDCODED
 
+    for blade in turbine.blades:
+        T0 = blade.GetTransform()
+        blade.SetTransform(dot(T,T0))
+     
+    rp = rail_place.RailPlace(psa)
+    turbine.place_rail(rp)
+    turbine.place_robot(rp)
+    organized_rays_list = organize_rays_in_parallels(DB, grid)
+    trajectory = path(organized_rays_list)
+    trajectory.execute(turbine, DB.load_blade(), threshold)
+    return trajectory
 
 def generate_linear_interpolation_rays(organized_rays, blade, threshold):
     new_rays = []
