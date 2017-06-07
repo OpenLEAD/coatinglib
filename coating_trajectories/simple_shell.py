8af08a5cd4df79ddb7fb0759d4b4611a8a78dae4
renan028 <renan028@gmail.com>
2017-06-07 15:37:46 -0300

    CHANGE simple sheel calls

--- coating_trajectories/simple_shell.py
+++ coating_trajectories/simple_shell.py
@@ -19,32 +19,22 @@ from openravepy import interfaces, IkParameterization, RaveCreateTrajectory
 import time
 import rail_place
 import mathtools
+from numpy import dot
+from numpy import linalg
+
 grid = 0
 robot = turb.robot
 manip = robot.GetActiveManipulator()
 robot.GetLink('Flame').Enable(False)
-DB = db.DB('LIP',turb)
+DB = db.DB('FACE',turb)
 
 T = array([[ 1.,  0.,  0.,  0.],
        [ 0.,  0.,  1.,  0.],
        [ 0., -1.,  0.,  0.],
        [ 0.,  0.,  0.,  1.]])
-
-psa = (1.0000000000000027, 1.0517976918168939, 0.30186829920226843)
-
-for blade in turb.blades:
-    blade.SetTransform(T)
-    
-rp = rail_place.RailPlace(psa)
-turb.place_rail(rp)
-turb.place_robot(rp)
+DB.T = T
+psa = (1.3000000000000029, 0.93948062679911604, 0.30186829920226832)
 
 threshold = 5e-2
-organized_rays_list = blade_coverage.organize_rays_in_parallels(DB, grid)
-joint_path, linear_interpolated_rays = blade_coverage.move_dijkstra(turb, DB.load_blade(), organized_rays_list, threshold)
-joint_path_2 = blade_coverage.refine_dijkstra(turb, joint_path, linear_interpolated_rays, threshold)
-new_joint_path = blade_coverage.smooth_joint_MLS(turb, joint_path_2)
-for path in new_joint_path:
-	for joint in path:
-		robot.SetDOFValues(joint)
-		time.sleep(0.05)
+path = blade_coverage.base_grid_validation(turb, psa, DB, grid, threshold = 5e-2)
+
