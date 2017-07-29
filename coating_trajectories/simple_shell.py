f870d8109d3bdc4258beb5f2164acdf35999d2c7
renan028 <renan028@gmail.com>
2017-07-29 14:35:38 -0300

    UPDATE test

--- coating_trajectories/simple_shell.py
+++ coating_trajectories/simple_shell.py
@@ -15,19 +15,19 @@ vis = visualizer.Visualizer(turb.env)
 import blade_coverage
 import db
 
-grid = 44
+grid = 1
 robot = turb.robot
 manip = robot.GetActiveManipulator()
 robot.GetLink('Flame').Enable(False)
-DB = db.DB('FACE',turb)
+DB = db.DB('LIP',turb)
 
-T = array([[ 1.        ,  0.        ,  0.        ,  0.        ],
-       [ 0.        ,  0.70710678,  0.70710678,  0.        ],
-       [ 0.        , -0.70710678,  0.70710678,  0.        ],
-       [ 0.        ,  0.        ,  0.        ,  1.        ]])
+T = array([[ 1.,  0.,  0.,  0.],
+       [ 0.,  0.,  1.,  0.],
+       [ 0., -1.,  0.,  0.],
+       [ 0.,  0.,  0.,  1.]])
 DB.T = T
-psa = (-0.99999999999999911, -0.52098509437189311, 1.5235987755982991)
+psa = (1.7500000000000036, 0.55169264516096073, 1.3490658503988662)
 
 threshold = 5e-2
-#path = blade_coverage.base_grid_validation(turb, psa, DB, grid, threshold)
+path = blade_coverage.base_grid_validation(turb, psa, DB, grid, threshold)
 
