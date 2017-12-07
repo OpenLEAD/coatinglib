aa9ee4893b918ca09d1642e927c2e35a3d9f28a0
renan028 <renan028@gmail.com>
2017-12-07 17:00:56 -0200

    fix paths

--- coating_trajectories/simple_shell.py
+++ coating_trajectories/simple_shell.py
@@ -5,9 +5,7 @@ from os.path import join, isfile, realpath
 from numpy import array
 from time import time
 
-dir_test = join(realpath('.'),'test')
-os.environ['OPENRAVE_DATA'] = str(dir_test)
-cfg = TurbineConfig.load('turbine_unittest.cfg','test')
+cfg = TurbineConfig.load('test/turbine_unittest.cfg')
 turbine = Turbine(cfg)
 
 import visualizer
@@ -20,14 +18,11 @@ grid = 7
 robot = turbine.robot
 manip = robot.GetActiveManipulator()
 robot.GetLink('Flame').Enable(False)
-DB = db.DB('FACE',turbine)
+DB = db.DB(join(os.environ['PYTHON_DATABASE'],'FACE'),turbine)
 
-T = array([[ 1.,  0.,  0.,  0.],
-           [ 0.,  0.25881905,  0.96592583,  0.],
-           [ 0., -0.96592583,  0.25881905,  0.],
-           [ 0.,  0.,  0.,  1.]])
+T = turbine.blades[3].GetTransform()
+
 DB.T = T
-
 psa = (1.8500000000000032, 0.53725006964517363, 0.82546707480056725)
 
 threshold = 5e-2
